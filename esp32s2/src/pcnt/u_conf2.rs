a6ead2db04a310261022f3eb92b03ef3b539b9ef
Christopher Liebman <liebman@zod.com>
2023-01-03 10:34:40 -0800

    rename some PCNT register fields to remove the unit number where the register is unit specific.

--- esp32s2/src/pcnt/u_conf2.rs
+++ esp32s2/src/pcnt/u_conf2.rs
@@ -34,40 +34,40 @@ impl From<crate::W<U_CONF2_SPEC>> for W {
         W(writer)
     }
 }
-#[doc = "Field `CNT_H_LIM_U0` reader - This register is used to configure the thr_h_lim value for unit %s."]
-pub type CNT_H_LIM_U0_R = crate::FieldReader<u16, u16>;
-#[doc = "Field `CNT_H_LIM_U0` writer - This register is used to configure the thr_h_lim value for unit %s."]
-pub type CNT_H_LIM_U0_W<'a, const O: u8> =
+#[doc = "Field `CNT_H_LIM_U` reader - This register is used to configure the thr_h_lim value for unit %s."]
+pub type CNT_H_LIM_U_R = crate::FieldReader<u16, u16>;
+#[doc = "Field `CNT_H_LIM_U` writer - This register is used to configure the thr_h_lim value for unit %s."]
+pub type CNT_H_LIM_U_W<'a, const O: u8> =
     crate::FieldWriter<'a, u32, U_CONF2_SPEC, u16, u16, 16, O>;
-#[doc = "Field `CNT_L_LIM_U0` reader - This register is used to configure the thr_l_lim value for unit %s."]
-pub type CNT_L_LIM_U0_R = crate::FieldReader<u16, u16>;
-#[doc = "Field `CNT_L_LIM_U0` writer - This register is used to configure the thr_l_lim value for unit %s."]
-pub type CNT_L_LIM_U0_W<'a, const O: u8> =
+#[doc = "Field `CNT_L_LIM_U` reader - This register is used to configure the thr_l_lim value for unit %s."]
+pub type CNT_L_LIM_U_R = crate::FieldReader<u16, u16>;
+#[doc = "Field `CNT_L_LIM_U` writer - This register is used to configure the thr_l_lim value for unit %s."]
+pub type CNT_L_LIM_U_W<'a, const O: u8> =
     crate::FieldWriter<'a, u32, U_CONF2_SPEC, u16, u16, 16, O>;
 impl R {
     #[doc = "Bits 0:15 - This register is used to configure the thr_h_lim value for unit %s."]
     #[inline(always)]
-    pub fn cnt_h_lim_u0(&self) -> CNT_H_LIM_U0_R {
-        CNT_H_LIM_U0_R::new((self.bits & 0xffff) as u16)
+    pub fn cnt_h_lim_u(&self) -> CNT_H_LIM_U_R {
+        CNT_H_LIM_U_R::new((self.bits & 0xffff) as u16)
     }
     #[doc = "Bits 16:31 - This register is used to configure the thr_l_lim value for unit %s."]
     #[inline(always)]
-    pub fn cnt_l_lim_u0(&self) -> CNT_L_LIM_U0_R {
-        CNT_L_LIM_U0_R::new(((self.bits >> 16) & 0xffff) as u16)
+    pub fn cnt_l_lim_u(&self) -> CNT_L_LIM_U_R {
+        CNT_L_LIM_U_R::new(((self.bits >> 16) & 0xffff) as u16)
     }
 }
 impl W {
     #[doc = "Bits 0:15 - This register is used to configure the thr_h_lim value for unit %s."]
     #[inline(always)]
     #[must_use]
-    pub fn cnt_h_lim_u0(&mut self) -> CNT_H_LIM_U0_W<0> {
-        CNT_H_LIM_U0_W::new(self)
+    pub fn cnt_h_lim_u(&mut self) -> CNT_H_LIM_U_W<0> {
+        CNT_H_LIM_U_W::new(self)
     }
     #[doc = "Bits 16:31 - This register is used to configure the thr_l_lim value for unit %s."]
     #[inline(always)]
     #[must_use]
-    pub fn cnt_l_lim_u0(&mut self) -> CNT_L_LIM_U0_W<16> {
-        CNT_L_LIM_U0_W::new(self)
+    pub fn cnt_l_lim_u(&mut self) -> CNT_L_LIM_U_W<16> {
+        CNT_L_LIM_U_W::new(self)
     }
     #[doc = "Writes raw bits to the register."]
     #[inline(always)]
