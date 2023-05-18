a62e61220485edcd30d870ee0dd31cbaaffc5e80
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-05-18 15:55:09 +0200

    esp32h2: Source changes of aligning PCNT names with other devices

--- esp32h2/src/pcnt/int_st.rs
+++ esp32h2/src/pcnt/int_st.rs
@@ -13,34 +13,34 @@ impl From<crate::R<INT_ST_SPEC>> for R {
         R(reader)
     }
 }
-#[doc = "Field `CNT_THR_EVENT_U0_INT_ST` reader - The masked interrupt status bit for the PCNT_CNT_THR_EVENT_U0_INT interrupt."]
-pub type CNT_THR_EVENT_U0_INT_ST_R = crate::BitReader<bool>;
-#[doc = "Field `CNT_THR_EVENT_U1_INT_ST` reader - The masked interrupt status bit for the PCNT_CNT_THR_EVENT_U1_INT interrupt."]
-pub type CNT_THR_EVENT_U1_INT_ST_R = crate::BitReader<bool>;
-#[doc = "Field `CNT_THR_EVENT_U2_INT_ST` reader - The masked interrupt status bit for the PCNT_CNT_THR_EVENT_U2_INT interrupt."]
-pub type CNT_THR_EVENT_U2_INT_ST_R = crate::BitReader<bool>;
-#[doc = "Field `CNT_THR_EVENT_U3_INT_ST` reader - The masked interrupt status bit for the PCNT_CNT_THR_EVENT_U3_INT interrupt."]
-pub type CNT_THR_EVENT_U3_INT_ST_R = crate::BitReader<bool>;
+#[doc = "Field `CNT_THR_EVENT_U0` reader - The masked interrupt status bit for the PCNT_CNT_THR_EVENT_U0_INT interrupt."]
+pub type CNT_THR_EVENT_U0_R = crate::BitReader<bool>;
+#[doc = "Field `CNT_THR_EVENT_U1` reader - The masked interrupt status bit for the PCNT_CNT_THR_EVENT_U1_INT interrupt."]
+pub type CNT_THR_EVENT_U1_R = crate::BitReader<bool>;
+#[doc = "Field `CNT_THR_EVENT_U2` reader - The masked interrupt status bit for the PCNT_CNT_THR_EVENT_U2_INT interrupt."]
+pub type CNT_THR_EVENT_U2_R = crate::BitReader<bool>;
+#[doc = "Field `CNT_THR_EVENT_U3` reader - The masked interrupt status bit for the PCNT_CNT_THR_EVENT_U3_INT interrupt."]
+pub type CNT_THR_EVENT_U3_R = crate::BitReader<bool>;
 impl R {
     #[doc = "Bit 0 - The masked interrupt status bit for the PCNT_CNT_THR_EVENT_U0_INT interrupt."]
     #[inline(always)]
-    pub fn cnt_thr_event_u0_int_st(&self) -> CNT_THR_EVENT_U0_INT_ST_R {
-        CNT_THR_EVENT_U0_INT_ST_R::new((self.bits & 1) != 0)
+    pub fn cnt_thr_event_u0(&self) -> CNT_THR_EVENT_U0_R {
+        CNT_THR_EVENT_U0_R::new((self.bits & 1) != 0)
     }
     #[doc = "Bit 1 - The masked interrupt status bit for the PCNT_CNT_THR_EVENT_U1_INT interrupt."]
     #[inline(always)]
-    pub fn cnt_thr_event_u1_int_st(&self) -> CNT_THR_EVENT_U1_INT_ST_R {
-        CNT_THR_EVENT_U1_INT_ST_R::new(((self.bits >> 1) & 1) != 0)
+    pub fn cnt_thr_event_u1(&self) -> CNT_THR_EVENT_U1_R {
+        CNT_THR_EVENT_U1_R::new(((self.bits >> 1) & 1) != 0)
     }
     #[doc = "Bit 2 - The masked interrupt status bit for the PCNT_CNT_THR_EVENT_U2_INT interrupt."]
     #[inline(always)]
-    pub fn cnt_thr_event_u2_int_st(&self) -> CNT_THR_EVENT_U2_INT_ST_R {
-        CNT_THR_EVENT_U2_INT_ST_R::new(((self.bits >> 2) & 1) != 0)
+    pub fn cnt_thr_event_u2(&self) -> CNT_THR_EVENT_U2_R {
+        CNT_THR_EVENT_U2_R::new(((self.bits >> 2) & 1) != 0)
     }
     #[doc = "Bit 3 - The masked interrupt status bit for the PCNT_CNT_THR_EVENT_U3_INT interrupt."]
     #[inline(always)]
-    pub fn cnt_thr_event_u3_int_st(&self) -> CNT_THR_EVENT_U3_INT_ST_R {
-        CNT_THR_EVENT_U3_INT_ST_R::new(((self.bits >> 3) & 1) != 0)
+    pub fn cnt_thr_event_u3(&self) -> CNT_THR_EVENT_U3_R {
+        CNT_THR_EVENT_U3_R::new(((self.bits >> 3) & 1) != 0)
     }
 }
 #[doc = "Interrupt status register\n\nThis register you can [`read`](crate::generic::Reg::read). See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [int_st](index.html) module"]
