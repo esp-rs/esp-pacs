0e21d4a21908b1d909d58a5fa0319f383ef1cfca
Jesse Braham <jesse@beta7.io>
2023-05-22 10:07:02 -0700

    Refactor `xtask` package to use subcommands rather than options

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -7,15 +7,12 @@ use std::{
 };
 
 use anyhow::{Error, Result};
-use clap::{Parser, ValueEnum};
-use form::util::create_directory_structure;
-use log::{info, warn, LevelFilter};
-use strum::{Display, EnumIter, EnumString, IntoEnumIterator};
-use svd2rust::{generate::device::render, load_from, util::build_rs, Config, Target};
-use svdtools::patch::process_file;
-use toml::Value;
+use clap::{Parser, Subcommand, ValueEnum};
+use strum::{Display, EnumIter, IntoEnumIterator};
+use svd2rust::{Config, Target};
+use toml_edit::Document;
 
-#[derive(Debug, Clone, Display, EnumIter, EnumString, ValueEnum)]
+#[derive(Debug, Clone, Display, EnumIter, ValueEnum)]
 #[strum(serialize_all = "lowercase")]
 enum Chip {
     Esp32,
@@ -28,88 +25,103 @@ enum Chip {
     Esp8266,
 }
 
+#[derive(Debug, Clone, Copy, Display, ValueEnum)]
+#[strum(serialize_all = "lowercase")]
+enum Version {
+    Major,
+    Minor,
+    Patch,
+}
+
 #[derive(Debug, Parser)]
 struct Cli {
-    /// Chip(s) to target
-    #[clap(value_enum)]
-    chips: Vec<Chip>,
-    /// Only patch the SVD, do not generate or build the PAC
-    #[clap(short, long, conflicts_with = "generate_only")]
-    patch_only: bool,
-    /// Patch the SVD and generate the PAC, but do not build it
-    #[clap(short, long, conflicts_with = "patch_only")]
-    generate_only: bool,
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Patch the specified package(s)'s SVD file
+    Patch {
+        /// Chip(s) to target
+        #[clap(value_enum, default_values_t = Chip::iter())]
+        chips: Vec<Chip>,
+    },
+
+    /// Generate the specified package(s)
+    ///
+    /// Additionally patches the releavant SVD(s) prior to generating the
+    /// package(s).
+    Generate {
+        /// Chip(s) to target
+        #[clap(value_enum, default_values_t = Chip::iter())]
+        chips: Vec<Chip>,
+    },
+
+    /// Build the specified package(s)
+    ///
+    /// Additionally patches the relevant SVD(s) and generates the relevant
+    /// package(s) prior to building the package(s).
+    Build {
+        /// Chip(s) to target
+        #[clap(value_enum, default_values_t = Chip::iter())]
+        chips: Vec<Chip>,
+    },
 }
 
 fn main() -> Result<()> {
     env_logger::Builder::new()
-        .filter_module("xtask", LevelFilter::Info)
+        .filter_module("xtask", log::LevelFilter::Info)
         .init();
-
-    let args = Cli::parse();
 
     // The directory containing the cargo manifest for the 'xtask' package is a
     // subdirectory within the cargo workspace.
     let workspace = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     let workspace = workspace.parent().unwrap().canonicalize()?;
 
-    // One or more chips can be specified as command-line arguments. If none are
-    // provided then the task will be run for all chips.
-    let chips = if !args.chips.is_empty() {
-        args.chips.iter().map(|c| c.to_string()).collect::<Vec<_>>()
-    } else {
-        Chip::iter().map(|c| c.to_string()).collect::<Vec<_>>()
-    };
-
-    for chip in chips {
-        info!("chip: {chip}");
-        let path = workspace.join(&chip);
-
-        // Always patch the SVD upon execution. If '--patch-only' has NOT been set, then
-        // additionally generate and format the PAC. If '--generate-only' has NOT been
-        // set, clean and build the PAC crate.
-        patch_svd(&chip, &path)?;
-
-        if !args.patch_only {
-            // Remove the src/ directory before we generate. If this fails we will assume
-            // it's because the directory does not exist.
-            if fs::remove_dir_all(&path.join("src")).is_err() {
-                warn!("unable to remove 'src/' directory");
-            }
-
-            generate_pac(&chip, &path)?;
-            format(&path)?;
-
-            if !args.generate_only {
-                clean(&path)?;
-                build(&path)?;
-            }
-        }
-
-        info!("finished with {chip}")
+    match Cli::parse().command {
+        Commands::Patch { chips } => chips
+            .iter()
+            .try_for_each(|chip| patch_svd(&workspace, chip)),
+        Commands::Generate { chips } => chips
+            .iter()
+            .try_for_each(|chip: &Chip| generate_package(&workspace, chip)),
+        Commands::Build { chips } => chips
+            .iter()
+            .try_for_each(|chip| build_package(&workspace, chip)),
     }
-
-    Ok(())
 }
 
-fn patch_svd(chip: &str, path: &Path) -> Result<()> {
-    let svd_path = path.join("svd");
+fn patch_svd(workspace: &Path, chip: &Chip) -> Result<()> {
+    log::info!("applying patches to SVD file for {chip}");
+
+    let svd_path = workspace.join(chip.to_string()).join("svd");
     let yaml_file = svd_path.join("patches").join(format!("{chip}.yaml"));
-    process_file(&yaml_file)?;
+    svdtools::patch::process_file(&yaml_file)?;
 
     let from = svd_path.join(format!("{chip}.base.svd.patched"));
     let to = svd_path.join(format!("{chip}.svd"));
-    info!("applying patches to SVD file");
     fs::rename(from, to)?;
 
     Ok(())
 }
 
-fn generate_pac(chip: &str, path: &Path) -> Result<()> {
-    let svd_file = path.join("svd").join(format!("{chip}.svd"));
-    info!("generating PAC from '{}'", svd_file.display());
+fn generate_package(workspace: &Path, chip: &Chip) -> Result<()> {
+    // Patch the SVD prior to generating the package:
+    patch_svd(workspace, chip)?;
 
-    let target = if get_build_target(path)?.contains("riscv") {
+    let path = workspace.join(chip.to_string());
+
+    // Remove the src/ directory before we generate. If this fails we will assume
+    // it's because the directory does not exist.
+    if fs::remove_dir_all(path.join("src")).is_err() {
+        log::warn!("unable to remove 'src/' directory");
+    }
+
+    let svd_file = path.join("svd").join(format!("{chip}.svd"));
+    log::info!("generating PAC from '{}'", svd_file.display());
+
+    let target = if build_target(&path)?.contains("riscv") {
         Target::RISCV
     } else {
         Target::XtensaLX
@@ -117,7 +129,7 @@ fn generate_pac(chip: &str, path: &Path) -> Result<()> {
 
     let config = Config {
         target,
-        output_dir: path.to_path_buf(),
+        output_dir: path.clone(),
         const_generic: true,
 
         ..match target {
@@ -130,10 +142,10 @@ fn generate_pac(chip: &str, path: &Path) -> Result<()> {
     };
 
     let input = fs::read_to_string(svd_file)?;
-    let device = load_from(&input, &config)?;
+    let device = svd2rust::load_from(&input, &config)?;
 
     let mut device_x = String::new();
-    let items = render(&device, &config, &mut device_x)?;
+    let items = svd2rust::generate::device::render(&device, &config, &mut device_x)?;
     let data = items.to_string();
 
     // Here we will sneakily patch in our own logo for the documentation :)
@@ -146,43 +158,28 @@ fn generate_pac(chip: &str, path: &Path) -> Result<()> {
     file.write_all(data.as_ref())?;
 
     writeln!(File::create(path.join("device.x"))?, "{}", device_x)?;
-    writeln!(File::create(path.join("build.rs"))?, "{}", build_rs())?;
+    writeln!(
+        File::create(path.join("build.rs"))?,
+        "{}",
+        svd2rust::util::build_rs()
+    )?;
+
+    format(&path)?;
 
     Ok(())
 }
 
-fn format(path: &PathBuf) -> Result<()> {
-    info!("running `form` and `rustfmt` on PAC");
-    let lib_file = path.join("lib.rs");
+fn build_package(workspace: &Path, chip: &Chip) -> Result<()> {
+    // Patch the SVD and generate the package prior to building:
+    generate_package(workspace, chip)?;
 
-    let base_dir = path.join("src");
-    let string_contents = fs::read_to_string(&lib_file)?;
-    create_directory_structure(base_dir, &string_contents).map_err(Error::msg)?;
+    let path = workspace.join(chip.to_string());
+    clean(&path)?;
 
-    fs::remove_file(&lib_file)?;
+    let channel = release_channel(&path)?;
+    let target = build_target(&path)?;
 
-    Command::new("cargo")
-        .arg("fmt")
-        .current_dir(path)
-        .output()?;
-
-    Ok(())
-}
-
-fn clean(path: &PathBuf) -> Result<()> {
-    Command::new("cargo")
-        .arg("clean")
-        .current_dir(path)
-        .output()?;
-
-    Ok(())
-}
-
-fn build(path: &PathBuf) -> Result<()> {
-    let channel = get_release_channel(path)?;
-    let target = get_build_target(path)?;
-
-    info!("building PAC using '{channel}' channel and targeting '{target}'");
+    log::info!("building PAC using '{channel}' channel and targeting '{target}'");
     Command::new("cargo")
         .args([
             &format!("+{channel}"),
@@ -200,30 +197,57 @@ fn build(path: &PathBuf) -> Result<()> {
     Ok(())
 }
 
-fn get_release_channel(path: &Path) -> Result<String> {
-    let toolchain_file = path.join("rust-toolchain.toml");
-    let channel = extract_toml_value(&toolchain_file, &["toolchain", "channel"])?;
-
-    Ok(channel)
-}
-
-fn get_build_target(path: &Path) -> Result<String> {
+fn build_target(path: &Path) -> Result<String> {
     let config_file = path.join(".cargo").join("config.toml");
     let target = extract_toml_value(&config_file, &["build", "target"])?;
 
     Ok(target)
 }
 
+fn release_channel(path: &Path) -> Result<String> {
+    let toolchain_file = path.join("rust-toolchain.toml");
+    let channel = extract_toml_value(&toolchain_file, &["toolchain", "channel"])?;
+
+    Ok(channel)
+}
+
 fn extract_toml_value(path: &Path, keys: &[&str]) -> Result<String> {
     let contents = fs::read_to_string(path)?;
-    let value = contents.parse::<Value>()?;
+    let document = contents.parse::<Document>()?;
 
-    let mut item = &value;
+    let mut item = document.as_item();
     for key in keys {
-        item = item.get(key).unwrap();
+        item = item.as_table().unwrap().get(key).unwrap();
     }
 
-    let item = item.to_string().replace('\"', "");
+    let item = item.to_string().trim().replace('"', "");
 
     Ok(item)
+}
+
+fn format(path: &Path) -> Result<()> {
+    log::info!("running `form` and `rustfmt` on PAC");
+    let lib_file = path.join("lib.rs");
+
+    let base_dir = path.join("src");
+    let string_contents = fs::read_to_string(&lib_file)?;
+    form::create_directory_structure(base_dir, &string_contents).map_err(Error::msg)?;
+
+    fs::remove_file(&lib_file)?;
+
+    Command::new("cargo")
+        .arg("fmt")
+        .current_dir(path)
+        .output()?;
+
+    Ok(())
+}
+
+fn clean(path: &Path) -> Result<()> {
+    Command::new("cargo")
+        .arg("clean")
+        .current_dir(path)
+        .output()?;
+
+    Ok(())
 }
