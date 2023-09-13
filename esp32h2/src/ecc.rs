2545b66f4c11dd78dc02a3c77c44a78fa5893beb
Juraj Sadel <juraj.sadel@espressif.com>
2023-09-13 22:09:56 +0800

    ESP32H2: Add Qx, Qy and Qz memory block to ECC peripheral (#156)
    * ESP32H2: add Qx, Qy and Qz memory blocks in ECC peripheral
    
    * ESP32H2: Regenerate ECC peripheral

--- esp32h2/src/ecc.rs
+++ esp32h2/src/ecc.rs
@@ -22,6 +22,12 @@ pub struct RegisterBlock {
     pub px_mem: [PX_MEM; 32],
     #[doc = "0x140..0x160 - The memory that stores Py."]
     pub py_mem: [PY_MEM; 32],
+    #[doc = "0x160..0x180 - The memory that stores Qx"]
+    pub qx_mem: [QX_MEM; 32],
+    #[doc = "0x180..0x1a0 - The memory that stores Qy"]
+    pub qy_mem: [QY_MEM; 32],
+    #[doc = "0x1a0..0x1c0 - The memory that stores Qz"]
+    pub qz_mem: [QZ_MEM; 32],
 }
 #[doc = "MULT_INT_RAW (r) register accessor: ECC interrupt raw register, valid in level.\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`mult_int_raw::R`].  See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mult_int_raw`] module"]
 pub type MULT_INT_RAW = crate::Reg<mult_int_raw::MULT_INT_RAW_SPEC>;
@@ -59,3 +65,15 @@ pub mod px_mem;
 pub type PY_MEM = crate::Reg<py_mem::PY_MEM_SPEC>;
 #[doc = "The memory that stores Py."]
 pub mod py_mem;
+#[doc = "QX_MEM (rw) register accessor: The memory that stores Qx\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`qx_mem::R`].  You can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`qx_mem::W`]. You can also [`modify`](crate::generic::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`qx_mem`] module"]
+pub type QX_MEM = crate::Reg<qx_mem::QX_MEM_SPEC>;
+#[doc = "The memory that stores Qx"]
+pub mod qx_mem;
+#[doc = "QY_MEM (rw) register accessor: The memory that stores Qy\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`qy_mem::R`].  You can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`qy_mem::W`]. You can also [`modify`](crate::generic::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`qy_mem`] module"]
+pub type QY_MEM = crate::Reg<qy_mem::QY_MEM_SPEC>;
+#[doc = "The memory that stores Qy"]
+pub mod qy_mem;
+#[doc = "QZ_MEM (rw) register accessor: The memory that stores Qz\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`qz_mem::R`].  You can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`qz_mem::W`]. You can also [`modify`](crate::generic::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`qz_mem`] module"]
+pub type QZ_MEM = crate::Reg<qz_mem::QZ_MEM_SPEC>;
+#[doc = "The memory that stores Qz"]
+pub mod qz_mem;
