088531fea3a6b4b63dc1eb57bd05d605766d2a99
Kirill Mikhailov <konnor1980@yandex.ru>
2023-09-19 15:59:32 +0200

    Fix `MULT_MODE` and `MODEXP_MODE` fields for `ESP32`
    After checking with the `TRM` for the ESP32 chip, I found out that the bit widths of these two registers do not match the documentation.

--- esp32/src/rsa/mult_mode.rs
+++ esp32/src/rsa/mult_mode.rs
@@ -3,21 +3,21 @@ pub type R = crate::R<MULT_MODE_SPEC>;
 #[doc = "Register `MULT_MODE` writer"]
 pub type W = crate::W<MULT_MODE_SPEC>;
 #[doc = "Field `MULT_MODE` reader - This register contains the mode of modular multiplication and multiplication."]
-pub type MULT_MODE_R = crate::BitReader;
+pub type MULT_MODE_R = crate::FieldReader;
 #[doc = "Field `MULT_MODE` writer - This register contains the mode of modular multiplication and multiplication."]
-pub type MULT_MODE_W<'a, REG, const O: u8> = crate::BitWriter<'a, REG, O>;
+pub type MULT_MODE_W<'a, REG, const O: u8> = crate::FieldWriter<'a, REG, 4, O>;
 impl R {
-    #[doc = "Bit 0 - This register contains the mode of modular multiplication and multiplication."]
+    #[doc = "Bits 0:3 - This register contains the mode of modular multiplication and multiplication."]
     #[inline(always)]
     pub fn mult_mode(&self) -> MULT_MODE_R {
-        MULT_MODE_R::new((self.bits & 1) != 0)
+        MULT_MODE_R::new((self.bits & 0x0f) as u8)
     }
 }
 #[cfg(feature = "impl-register-debug")]
 impl core::fmt::Debug for R {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
         f.debug_struct("MULT_MODE")
-            .field("mult_mode", &format_args!("{}", self.mult_mode().bit()))
+            .field("mult_mode", &format_args!("{}", self.mult_mode().bits()))
             .finish()
     }
 }
@@ -28,7 +28,7 @@ impl core::fmt::Debug for crate::generic::Reg<MULT_MODE_SPEC> {
     }
 }
 impl W {
-    #[doc = "Bit 0 - This register contains the mode of modular multiplication and multiplication."]
+    #[doc = "Bits 0:3 - This register contains the mode of modular multiplication and multiplication."]
     #[inline(always)]
     #[must_use]
     pub fn mult_mode(&mut self) -> MULT_MODE_W<MULT_MODE_SPEC, 0> {
