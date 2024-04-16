e363df8e297d40e208a9e82fdc1be63420883fa8
Scott Mabin <scott@mabez.dev>
2024-04-16 23:54:51 +0100

    Fix esp32s2 systimer raw register naming

--- esp32s2/src/systimer/int_raw.rs
+++ esp32s2/src/systimer/int_raw.rs
@@ -1,35 +1,35 @@
 #[doc = "Register `INT_RAW` reader"]
 pub type R = crate::R<INT_RAW_SPEC>;
-#[doc = "Field `INT0_RAW` reader - Interrupt raw bit of system timer target 0."]
-pub type INT0_RAW_R = crate::BitReader;
-#[doc = "Field `INT1_RAW` reader - Interrupt raw bit of system timer target 1."]
-pub type INT1_RAW_R = crate::BitReader;
-#[doc = "Field `INT2_RAW` reader - Interrupt raw bit of system timer target 2."]
-pub type INT2_RAW_R = crate::BitReader;
+#[doc = "Field `TARGET0` reader - Interrupt raw bit of system timer target 0."]
+pub type TARGET0_R = crate::BitReader;
+#[doc = "Field `TARGET1` reader - Interrupt raw bit of system timer target 1."]
+pub type TARGET1_R = crate::BitReader;
+#[doc = "Field `TARGET2` reader - Interrupt raw bit of system timer target 2."]
+pub type TARGET2_R = crate::BitReader;
 impl R {
     #[doc = "Bit 0 - Interrupt raw bit of system timer target 0."]
     #[inline(always)]
-    pub fn int0_raw(&self) -> INT0_RAW_R {
-        INT0_RAW_R::new((self.bits & 1) != 0)
+    pub fn target0(&self) -> TARGET0_R {
+        TARGET0_R::new((self.bits & 1) != 0)
     }
     #[doc = "Bit 1 - Interrupt raw bit of system timer target 1."]
     #[inline(always)]
-    pub fn int1_raw(&self) -> INT1_RAW_R {
-        INT1_RAW_R::new(((self.bits >> 1) & 1) != 0)
+    pub fn target1(&self) -> TARGET1_R {
+        TARGET1_R::new(((self.bits >> 1) & 1) != 0)
     }
     #[doc = "Bit 2 - Interrupt raw bit of system timer target 2."]
     #[inline(always)]
-    pub fn int2_raw(&self) -> INT2_RAW_R {
-        INT2_RAW_R::new(((self.bits >> 2) & 1) != 0)
+    pub fn target2(&self) -> TARGET2_R {
+        TARGET2_R::new(((self.bits >> 2) & 1) != 0)
     }
 }
 #[cfg(feature = "impl-register-debug")]
 impl core::fmt::Debug for R {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
         f.debug_struct("INT_RAW")
-            .field("int0_raw", &format_args!("{}", self.int0_raw().bit()))
-            .field("int1_raw", &format_args!("{}", self.int1_raw().bit()))
-            .field("int2_raw", &format_args!("{}", self.int2_raw().bit()))
+            .field("target0", &format_args!("{}", self.target0().bit()))
+            .field("target1", &format_args!("{}", self.target1().bit()))
+            .field("target2", &format_args!("{}", self.target2().bit()))
             .finish()
     }
 }
