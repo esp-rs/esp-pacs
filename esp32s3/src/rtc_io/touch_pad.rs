045d2d93370af35743274dae7b07e0f3c4dbf8ae
Kirill Mikhailov <62840029+playfulFence@users.noreply.github.com>
2024-08-21 17:14:07 +0200

    ESP32S3: Make an array out of touch_pad (#273)
    * ESP32: Make an array out of `touch_pad`
    
    * + esp32s3
    
    * Revert `esp32` changes

--- esp32s3/src/rtc_io/touch_pad.rs
+++ esp32s3/src/rtc_io/touch_pad.rs
@@ -1,7 +1,7 @@
-#[doc = "Register `TOUCH_PAD1` reader"]
-pub type R = crate::R<TOUCH_PAD1_SPEC>;
-#[doc = "Register `TOUCH_PAD1` writer"]
-pub type W = crate::W<TOUCH_PAD1_SPEC>;
+#[doc = "Register `TOUCH_PAD%s` reader"]
+pub type R = crate::R<TOUCH_PAD_SPEC>;
+#[doc = "Register `TOUCH_PAD%s` writer"]
+pub type W = crate::W<TOUCH_PAD_SPEC>;
 #[doc = "Field `FUN_IE` reader - input enable in work mode"]
 pub type FUN_IE_R = crate::BitReader;
 #[doc = "Field `FUN_IE` writer - input enable in work mode"]
@@ -115,7 +115,7 @@ impl R {
 #[cfg(feature = "impl-register-debug")]
 impl core::fmt::Debug for R {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-        f.debug_struct("TOUCH_PAD1")
+        f.debug_struct("TOUCH_PAD")
             .field("fun_ie", &self.fun_ie())
             .field("slp_oe", &self.slp_oe())
             .field("slp_ie", &self.slp_ie())
@@ -135,90 +135,90 @@ impl W {
     #[doc = "Bit 13 - input enable in work mode"]
     #[inline(always)]
     #[must_use]
-    pub fn fun_ie(&mut self) -> FUN_IE_W<TOUCH_PAD1_SPEC> {
+    pub fn fun_ie(&mut self) -> FUN_IE_W<TOUCH_PAD_SPEC> {
         FUN_IE_W::new(self, 13)
     }
     #[doc = "Bit 14 - output enable in sleep mode"]
     #[inline(always)]
     #[must_use]
-    pub fn slp_oe(&mut self) -> SLP_OE_W<TOUCH_PAD1_SPEC> {
+    pub fn slp_oe(&mut self) -> SLP_OE_W<TOUCH_PAD_SPEC> {
         SLP_OE_W::new(self, 14)
     }
     #[doc = "Bit 15 - input enable in sleep mode"]
     #[inline(always)]
     #[must_use]
-    pub fn slp_ie(&mut self) -> SLP_IE_W<TOUCH_PAD1_SPEC> {
+    pub fn slp_ie(&mut self) -> SLP_IE_W<TOUCH_PAD_SPEC> {
         SLP_IE_W::new(self, 15)
     }
     #[doc = "Bit 16 - 1: enable sleep mode during sleep,0: no sleep mode"]
     #[inline(always)]
     #[must_use]
-    pub fn slp_sel(&mut self) -> SLP_SEL_W<TOUCH_PAD1_SPEC> {
+    pub fn slp_sel(&mut self) -> SLP_SEL_W<TOUCH_PAD_SPEC> {
         SLP_SEL_W::new(self, 16)
     }
     #[doc = "Bits 17:18 - function sel"]
     #[inline(always)]
     #[must_use]
-    pub fn fun_sel(&mut self) -> FUN_SEL_W<TOUCH_PAD1_SPEC> {
+    pub fn fun_sel(&mut self) -> FUN_SEL_W<TOUCH_PAD_SPEC> {
         FUN_SEL_W::new(self, 17)
     }
     #[doc = "Bit 19 - 1: use RTC GPIO,0: use digital GPIO"]
     #[inline(always)]
     #[must_use]
-    pub fn mux_sel(&mut self) -> MUX_SEL_W<TOUCH_PAD1_SPEC> {
+    pub fn mux_sel(&mut self) -> MUX_SEL_W<TOUCH_PAD_SPEC> {
         MUX_SEL_W::new(self, 19)
     }
     #[doc = "Bit 20 - TOUCH_XPD"]
     #[inline(always)]
     #[must_use]
-    pub fn xpd(&mut self) -> XPD_W<TOUCH_PAD1_SPEC> {
+    pub fn xpd(&mut self) -> XPD_W<TOUCH_PAD_SPEC> {
         XPD_W::new(self, 20)
     }
     #[doc = "Bit 21 - TOUCH_TIE_OPT"]
     #[inline(always)]
     #[must_use]
-    pub fn tie_opt(&mut self) -> TIE_OPT_W<TOUCH_PAD1_SPEC> {
+    pub fn tie_opt(&mut self) -> TIE_OPT_W<TOUCH_PAD_SPEC> {
         TIE_OPT_W::new(self, 21)
     }
     #[doc = "Bit 22 - TOUCH_START"]
     #[inline(always)]
     #[must_use]
-    pub fn start(&mut self) -> START_W<TOUCH_PAD1_SPEC> {
+    pub fn start(&mut self) -> START_W<TOUCH_PAD_SPEC> {
         START_W::new(self, 22)
     }
     #[doc = "Bit 27 - RUE"]
     #[inline(always)]
     #[must_use]
-    pub fn rue(&mut self) -> RUE_W<TOUCH_PAD1_SPEC> {
+    pub fn rue(&mut self) -> RUE_W<TOUCH_PAD_SPEC> {
         RUE_W::new(self, 27)
     }
     #[doc = "Bit 28 - RDE"]
     #[inline(always)]
     #[must_use]
-    pub fn rde(&mut self) -> RDE_W<TOUCH_PAD1_SPEC> {
+    pub fn rde(&mut self) -> RDE_W<TOUCH_PAD_SPEC> {
         RDE_W::new(self, 28)
     }
     #[doc = "Bits 29:30 - DRV"]
     #[inline(always)]
     #[must_use]
-    pub fn drv(&mut self) -> DRV_W<TOUCH_PAD1_SPEC> {
+    pub fn drv(&mut self) -> DRV_W<TOUCH_PAD_SPEC> {
         DRV_W::new(self, 29)
     }
 }
-#[doc = "configure RTC PAD1\n\nYou can [`read`](crate::Reg::read) this register and get [`touch_pad1::R`](R). You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`touch_pad1::W`](W). You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
-pub struct TOUCH_PAD1_SPEC;
-impl crate::RegisterSpec for TOUCH_PAD1_SPEC {
+#[doc = "configure RTC PAD%s\n\nYou can [`read`](crate::Reg::read) this register and get [`touch_pad::R`](R). You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`touch_pad::W`](W). You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
+pub struct TOUCH_PAD_SPEC;
+impl crate::RegisterSpec for TOUCH_PAD_SPEC {
     type Ux = u32;
 }
-#[doc = "`read()` method returns [`touch_pad1::R`](R) reader structure"]
-impl crate::Readable for TOUCH_PAD1_SPEC {}
-#[doc = "`write(|w| ..)` method takes [`touch_pad1::W`](W) writer structure"]
-impl crate::Writable for TOUCH_PAD1_SPEC {
+#[doc = "`read()` method returns [`touch_pad::R`](R) reader structure"]
+impl crate::Readable for TOUCH_PAD_SPEC {}
+#[doc = "`write(|w| ..)` method takes [`touch_pad::W`](W) writer structure"]
+impl crate::Writable for TOUCH_PAD_SPEC {
     type Safety = crate::Unsafe;
     const ZERO_TO_MODIFY_FIELDS_BITMAP: u32 = 0;
     const ONE_TO_MODIFY_FIELDS_BITMAP: u32 = 0;
 }
-#[doc = "`reset()` method sets TOUCH_PAD1 to value 0x4800_0000"]
-impl crate::Resettable for TOUCH_PAD1_SPEC {
-    const RESET_VALUE: u32 = 0x4800_0000;
+#[doc = "`reset()` method sets TOUCH_PAD%s to value 0x5000_0000"]
+impl crate::Resettable for TOUCH_PAD_SPEC {
+    const RESET_VALUE: u32 = 0x5000_0000;
 }
