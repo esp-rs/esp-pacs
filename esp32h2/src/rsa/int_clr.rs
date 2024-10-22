0f900a364ba0d41cf056e3ecb6bd62751ca2612f
Jesse Braham <jesse@beta7.io>
2024-10-22 13:00:49 +0200

    More RSA interrupt clear cleanup I guess

--- esp32h2/src/rsa/int_clr.rs
+++ esp32h2/src/rsa/int_clr.rs
@@ -1,7 +1,7 @@
 #[doc = "Register `INT_CLR` writer"]
 pub type W = crate::W<INT_CLR_SPEC>;
-#[doc = "Field `CLEAR_INTERRUPT` writer - Write 1 to clear the RSA interrupt."]
-pub type CLEAR_INTERRUPT_W<'a, REG> = crate::BitWriter<'a, REG>;
+#[doc = "Field `INT_CLR` writer - Write 1 to clear the RSA interrupt."]
+pub type INT_CLR_W<'a, REG> = crate::BitWriter<'a, REG>;
 #[cfg(feature = "impl-register-debug")]
 impl core::fmt::Debug for crate::generic::Reg<INT_CLR_SPEC> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
@@ -12,8 +12,8 @@ impl W {
     #[doc = "Bit 0 - Write 1 to clear the RSA interrupt."]
     #[inline(always)]
     #[must_use]
-    pub fn clear_interrupt(&mut self) -> CLEAR_INTERRUPT_W<INT_CLR_SPEC> {
-        CLEAR_INTERRUPT_W::new(self, 0)
+    pub fn int_clr(&mut self) -> INT_CLR_W<INT_CLR_SPEC> {
+        INT_CLR_W::new(self, 0)
     }
 }
 #[doc = "Clears RSA interrupt\n\nYou can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`int_clr::W`](W). See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
