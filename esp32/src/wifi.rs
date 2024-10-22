05455ccf51c756de335012456b3ca500c711d6f8
Frostie314159 <frostie.neuenhausen@gmail.com>
2024-10-22 20:20:08 +0200

    Added some statistic registers.

--- esp32/src/wifi.rs
+++ esp32/src/wifi.rs
@@ -10,20 +10,34 @@ pub struct RegisterBlock {
     rx_descr_last: RX_DESCR_LAST,
     _reserved5: [u8; 0x44],
     unknown_rx_policy: [UNKNOWN_RX_POLICY; 2],
-    _reserved6: [u8; 0x0b68],
+    _reserved6: [u8; 0x01ec],
+    hw_stat_rx_success: HW_STAT_RX_SUCCESS,
+    hw_stat_rx_end: HW_STAT_RX_END,
+    _reserved8: [u8; 0x04],
+    hw_stat_hop_err: HW_STAT_HOP_ERR,
+    hw_stat_full2: HW_STAT_FULL2,
+    _reserved10: [u8; 0x0968],
     wifi_int_status: WIFI_INT_STATUS,
     wifi_int_clear: WIFI_INT_CLEAR,
-    _reserved8: [u8; 0x68],
+    _reserved12: [u8; 0x68],
     ctrl: CTRL,
     tx_error_clear: TX_ERROR_CLEAR,
     tx_error_status: TX_ERROR_STATUS,
     tx_complete_clear: TX_COMPLETE_CLEAR,
     tx_complete_status: TX_COMPLETE_STATUS,
-    _reserved13: [u8; 0x30],
+    _reserved17: [u8; 0x30],
     tx_slot_config: [TX_SLOT_CONFIG; 5],
-    _reserved14: [u8; 0x0444],
+    _reserved18: [u8; 0x34],
+    hw_stat_tx_rts: HW_STAT_TX_RTS,
+    hw_stat_tx_cts: HW_STAT_TX_CTS,
+    hw_stat_tx_ack: HW_STAT_TX_ACK,
+    hw_stat_trcts: HW_STAT_TRCTS,
+    hw_stat_trigger: HW_STAT_TRIGGER,
+    hw_stat_tx_hung: HW_STAT_TX_HUNG,
+    hw_stat_panic: HW_STAT_PANIC,
+    _reserved25: [u8; 0x03f4],
     tx_slot_parameters: [TX_SLOT_PARAMETERS; 5],
-    _reserved15: [u8; 0x016c],
+    _reserved26: [u8; 0x016c],
     crypto_key_entry: [CRYPTO_KEY_ENTRY; 16],
 }
 impl RegisterBlock {
@@ -68,6 +82,26 @@ impl RegisterBlock {
     #[inline(always)]
     pub fn unknown_rx_policy_iter(&self) -> impl Iterator<Item = &UNKNOWN_RX_POLICY> {
         self.unknown_rx_policy.iter()
+    }
+    #[doc = "0x2cc - "]
+    #[inline(always)]
+    pub const fn hw_stat_rx_success(&self) -> &HW_STAT_RX_SUCCESS {
+        &self.hw_stat_rx_success
+    }
+    #[doc = "0x2d0 - "]
+    #[inline(always)]
+    pub const fn hw_stat_rx_end(&self) -> &HW_STAT_RX_END {
+        &self.hw_stat_rx_end
+    }
+    #[doc = "0x2d8 - "]
+    #[inline(always)]
+    pub const fn hw_stat_hop_err(&self) -> &HW_STAT_HOP_ERR {
+        &self.hw_stat_hop_err
+    }
+    #[doc = "0x2dc - "]
+    #[inline(always)]
+    pub const fn hw_stat_full2(&self) -> &HW_STAT_FULL2 {
+        &self.hw_stat_full2
     }
     #[doc = "0xc48 - Interrupt status of WIFI peripheral"]
     #[inline(always)]
@@ -115,6 +149,41 @@ impl RegisterBlock {
     pub fn tx_slot_config_iter(&self) -> impl Iterator<Item = &TX_SLOT_CONFIG> {
         self.tx_slot_config.iter()
     }
+    #[doc = "0xd58 - "]
+    #[inline(always)]
+    pub const fn hw_stat_tx_rts(&self) -> &HW_STAT_TX_RTS {
+        &self.hw_stat_tx_rts
+    }
+    #[doc = "0xd5c - "]
+    #[inline(always)]
+    pub const fn hw_stat_tx_cts(&self) -> &HW_STAT_TX_CTS {
+        &self.hw_stat_tx_cts
+    }
+    #[doc = "0xd60 - "]
+    #[inline(always)]
+    pub const fn hw_stat_tx_ack(&self) -> &HW_STAT_TX_ACK {
+        &self.hw_stat_tx_ack
+    }
+    #[doc = "0xd64 - "]
+    #[inline(always)]
+    pub const fn hw_stat_trcts(&self) -> &HW_STAT_TRCTS {
+        &self.hw_stat_trcts
+    }
+    #[doc = "0xd68 - "]
+    #[inline(always)]
+    pub const fn hw_stat_trigger(&self) -> &HW_STAT_TRIGGER {
+        &self.hw_stat_trigger
+    }
+    #[doc = "0xd6c - "]
+    #[inline(always)]
+    pub const fn hw_stat_tx_hung(&self) -> &HW_STAT_TX_HUNG {
+        &self.hw_stat_tx_hung
+    }
+    #[doc = "0xd70 - "]
+    #[inline(always)]
+    pub const fn hw_stat_panic(&self) -> &HW_STAT_PANIC {
+        &self.hw_stat_panic
+    }
     #[doc = "0x1168..0x1294 - Used to set transmission parameters for the slot"]
     #[inline(always)]
     pub const fn tx_slot_parameters(&self, n: usize) -> &TX_SLOT_PARAMETERS {
@@ -158,6 +227,22 @@ pub mod rx_descr_last;
 pub type UNKNOWN_RX_POLICY = crate::Reg<unknown_rx_policy::UNKNOWN_RX_POLICY_SPEC>;
 #[doc = ""]
 pub mod unknown_rx_policy;
+#[doc = "HW_STAT_RX_SUCCESS (rw) register accessor: \n\nYou can [`read`](crate::Reg::read) this register and get [`hw_stat_rx_success::R`]. You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`hw_stat_rx_success::W`]. You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@hw_stat_rx_success`] module"]
+pub type HW_STAT_RX_SUCCESS = crate::Reg<hw_stat_rx_success::HW_STAT_RX_SUCCESS_SPEC>;
+#[doc = ""]
+pub mod hw_stat_rx_success;
+#[doc = "HW_STAT_RX_END (rw) register accessor: \n\nYou can [`read`](crate::Reg::read) this register and get [`hw_stat_rx_end::R`]. You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`hw_stat_rx_end::W`]. You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@hw_stat_rx_end`] module"]
+pub type HW_STAT_RX_END = crate::Reg<hw_stat_rx_end::HW_STAT_RX_END_SPEC>;
+#[doc = ""]
+pub mod hw_stat_rx_end;
+#[doc = "HW_STAT_HOP_ERR (rw) register accessor: \n\nYou can [`read`](crate::Reg::read) this register and get [`hw_stat_hop_err::R`]. You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`hw_stat_hop_err::W`]. You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@hw_stat_hop_err`] module"]
+pub type HW_STAT_HOP_ERR = crate::Reg<hw_stat_hop_err::HW_STAT_HOP_ERR_SPEC>;
+#[doc = ""]
+pub mod hw_stat_hop_err;
+#[doc = "HW_STAT_FULL2 (rw) register accessor: \n\nYou can [`read`](crate::Reg::read) this register and get [`hw_stat_full2::R`]. You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`hw_stat_full2::W`]. You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@hw_stat_full2`] module"]
+pub type HW_STAT_FULL2 = crate::Reg<hw_stat_full2::HW_STAT_FULL2_SPEC>;
+#[doc = ""]
+pub mod hw_stat_full2;
 #[doc = "WIFI_INT_STATUS (rw) register accessor: Interrupt status of WIFI peripheral\n\nYou can [`read`](crate::Reg::read) this register and get [`wifi_int_status::R`]. You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`wifi_int_status::W`]. You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@wifi_int_status`] module"]
 pub type WIFI_INT_STATUS = crate::Reg<wifi_int_status::WIFI_INT_STATUS_SPEC>;
 #[doc = "Interrupt status of WIFI peripheral"]
@@ -186,6 +271,34 @@ pub mod tx_complete_clear;
 pub type TX_COMPLETE_STATUS = crate::Reg<tx_complete_status::TX_COMPLETE_STATUS_SPEC>;
 #[doc = "Completion status of a slot"]
 pub mod tx_complete_status;
+#[doc = "HW_STAT_TX_RTS (rw) register accessor: \n\nYou can [`read`](crate::Reg::read) this register and get [`hw_stat_tx_rts::R`]. You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`hw_stat_tx_rts::W`]. You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@hw_stat_tx_rts`] module"]
+pub type HW_STAT_TX_RTS = crate::Reg<hw_stat_tx_rts::HW_STAT_TX_RTS_SPEC>;
+#[doc = ""]
+pub mod hw_stat_tx_rts;
+#[doc = "HW_STAT_TX_CTS (rw) register accessor: \n\nYou can [`read`](crate::Reg::read) this register and get [`hw_stat_tx_cts::R`]. You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`hw_stat_tx_cts::W`]. You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@hw_stat_tx_cts`] module"]
+pub type HW_STAT_TX_CTS = crate::Reg<hw_stat_tx_cts::HW_STAT_TX_CTS_SPEC>;
+#[doc = ""]
+pub mod hw_stat_tx_cts;
+#[doc = "HW_STAT_TX_ACK (rw) register accessor: \n\nYou can [`read`](crate::Reg::read) this register and get [`hw_stat_tx_ack::R`]. You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`hw_stat_tx_ack::W`]. You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@hw_stat_tx_ack`] module"]
+pub type HW_STAT_TX_ACK = crate::Reg<hw_stat_tx_ack::HW_STAT_TX_ACK_SPEC>;
+#[doc = ""]
+pub mod hw_stat_tx_ack;
+#[doc = "HW_STAT_TRCTS (rw) register accessor: \n\nYou can [`read`](crate::Reg::read) this register and get [`hw_stat_trcts::R`]. You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`hw_stat_trcts::W`]. You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@hw_stat_trcts`] module"]
+pub type HW_STAT_TRCTS = crate::Reg<hw_stat_trcts::HW_STAT_TRCTS_SPEC>;
+#[doc = ""]
+pub mod hw_stat_trcts;
+#[doc = "HW_STAT_TRIGGER (rw) register accessor: \n\nYou can [`read`](crate::Reg::read) this register and get [`hw_stat_trigger::R`]. You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`hw_stat_trigger::W`]. You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@hw_stat_trigger`] module"]
+pub type HW_STAT_TRIGGER = crate::Reg<hw_stat_trigger::HW_STAT_TRIGGER_SPEC>;
+#[doc = ""]
+pub mod hw_stat_trigger;
+#[doc = "HW_STAT_TX_HUNG (rw) register accessor: \n\nYou can [`read`](crate::Reg::read) this register and get [`hw_stat_tx_hung::R`]. You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`hw_stat_tx_hung::W`]. You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@hw_stat_tx_hung`] module"]
+pub type HW_STAT_TX_HUNG = crate::Reg<hw_stat_tx_hung::HW_STAT_TX_HUNG_SPEC>;
+#[doc = ""]
+pub mod hw_stat_tx_hung;
+#[doc = "HW_STAT_PANIC (rw) register accessor: \n\nYou can [`read`](crate::Reg::read) this register and get [`hw_stat_panic::R`]. You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`hw_stat_panic::W`]. You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@hw_stat_panic`] module"]
+pub type HW_STAT_PANIC = crate::Reg<hw_stat_panic::HW_STAT_PANIC_SPEC>;
+#[doc = ""]
+pub mod hw_stat_panic;
 #[doc = "Filter banks for frame reception. Bank zero is for the BSSID and bank one for the RA. Each filter bank has registers for two interfaces."]
 pub use self::filter_bank::FILTER_BANK;
 #[doc = r"Cluster"]
