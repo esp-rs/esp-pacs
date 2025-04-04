29b55084c365584aec4861d792d44675e641f30c
Björn Quentin <bjoernQ@users.noreply.github.com>
2025-04-04 14:13:18 +0200

    Remove the `_I2C` postfix from I2C-ST timeout fields

--- esp32s3/src/i2c0/scl_st_time_out.rs
+++ esp32s3/src/i2c0/scl_st_time_out.rs
@@ -2,30 +2,30 @@
 pub type R = crate::R<SCL_ST_TIME_OUT_SPEC>;
 #[doc = "Register `SCL_ST_TIME_OUT` writer"]
 pub type W = crate::W<SCL_ST_TIME_OUT_SPEC>;
-#[doc = "Field `SCL_ST_TO_I2C` reader - The threshold value of SCL_FSM state unchanged period. It should be o more than 23"]
-pub type SCL_ST_TO_I2C_R = crate::FieldReader;
-#[doc = "Field `SCL_ST_TO_I2C` writer - The threshold value of SCL_FSM state unchanged period. It should be o more than 23"]
-pub type SCL_ST_TO_I2C_W<'a, REG> = crate::FieldWriter<'a, REG, 5>;
+#[doc = "Field `SCL_ST_TO` reader - The threshold value of SCL_FSM state unchanged period. It should be o more than 23"]
+pub type SCL_ST_TO_R = crate::FieldReader;
+#[doc = "Field `SCL_ST_TO` writer - The threshold value of SCL_FSM state unchanged period. It should be o more than 23"]
+pub type SCL_ST_TO_W<'a, REG> = crate::FieldWriter<'a, REG, 5>;
 impl R {
     #[doc = "Bits 0:4 - The threshold value of SCL_FSM state unchanged period. It should be o more than 23"]
     #[inline(always)]
-    pub fn scl_st_to_i2c(&self) -> SCL_ST_TO_I2C_R {
-        SCL_ST_TO_I2C_R::new((self.bits & 0x1f) as u8)
+    pub fn scl_st_to(&self) -> SCL_ST_TO_R {
+        SCL_ST_TO_R::new((self.bits & 0x1f) as u8)
     }
 }
 #[cfg(feature = "impl-register-debug")]
 impl core::fmt::Debug for R {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
         f.debug_struct("SCL_ST_TIME_OUT")
-            .field("scl_st_to_i2c", &self.scl_st_to_i2c())
+            .field("scl_st_to", &self.scl_st_to())
             .finish()
     }
 }
 impl W {
     #[doc = "Bits 0:4 - The threshold value of SCL_FSM state unchanged period. It should be o more than 23"]
     #[inline(always)]
-    pub fn scl_st_to_i2c(&mut self) -> SCL_ST_TO_I2C_W<SCL_ST_TIME_OUT_SPEC> {
-        SCL_ST_TO_I2C_W::new(self, 0)
+    pub fn scl_st_to(&mut self) -> SCL_ST_TO_W<SCL_ST_TIME_OUT_SPEC> {
+        SCL_ST_TO_W::new(self, 0)
     }
 }
 #[doc = "SCL status time out register\n\nYou can [`read`](crate::Reg::read) this register and get [`scl_st_time_out::R`](R). You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`scl_st_time_out::W`](W). You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
