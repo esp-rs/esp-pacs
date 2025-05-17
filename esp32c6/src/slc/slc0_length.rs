0aadbcdaae3314a32d75c3830e6aca36fdef9ca3
rmsyn <github@weathered-steel.dev>
2025-05-17 07:34:45 +0000

    esp32c6: re-generate for SLC0_LENGTH name fix
    Re-generates the `esp32c6` PAC with the SLC0_LENGTH register name fix.

--- esp32c6/src/slc/slc0_length.rs
+++ esp32c6/src/slc/slc0_length.rs
@@ -1,5 +1,5 @@
-#[doc = "Register `SLC10_LENGTH` reader"]
-pub type R = crate::R<SLC10_LENGTH_SPEC>;
+#[doc = "Register `SLC0_LENGTH` reader"]
+pub type R = crate::R<SLC0_LENGTH_SPEC>;
 #[doc = "Field `SDIO_SLC0_LEN` reader - Represents the accumulated length of data that the slave wants to send."]
 pub type SDIO_SLC0_LEN_R = crate::FieldReader<u32>;
 impl R {
@@ -12,19 +12,19 @@ impl R {
 #[cfg(feature = "impl-register-debug")]
 impl core::fmt::Debug for R {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-        f.debug_struct("SLC10_LENGTH")
+        f.debug_struct("SLC0_LENGTH")
             .field("sdio_slc0_len", &self.sdio_slc0_len())
             .finish()
     }
 }
-#[doc = "Length of transmitting packets\n\nYou can [`read`](crate::Reg::read) this register and get [`slc10_length::R`](R). See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
-pub struct SLC10_LENGTH_SPEC;
-impl crate::RegisterSpec for SLC10_LENGTH_SPEC {
+#[doc = "Length of transmitting packets\n\nYou can [`read`](crate::Reg::read) this register and get [`slc0_length::R`](R). See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
+pub struct SLC0_LENGTH_SPEC;
+impl crate::RegisterSpec for SLC0_LENGTH_SPEC {
     type Ux = u32;
 }
-#[doc = "`read()` method returns [`slc10_length::R`](R) reader structure"]
-impl crate::Readable for SLC10_LENGTH_SPEC {}
-#[doc = "`reset()` method sets SLC10_LENGTH to value 0"]
-impl crate::Resettable for SLC10_LENGTH_SPEC {
+#[doc = "`read()` method returns [`slc0_length::R`](R) reader structure"]
+impl crate::Readable for SLC0_LENGTH_SPEC {}
+#[doc = "`reset()` method sets SLC0_LENGTH to value 0"]
+impl crate::Resettable for SLC0_LENGTH_SPEC {
     const RESET_VALUE: u32 = 0;
 }
