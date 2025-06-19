7707ae72e50dfed7aea16da89cc36c7ff638ff37
Dániel Buga <bugadani@gmail.com>
2025-06-19 13:13:56 +0200

    Collect S2 IO_MUX pin registers into an array (#337)

--- esp32s2/src/io_mux/gpio.rs
+++ esp32s2/src/io_mux/gpio.rs
@@ -1,7 +1,7 @@
-#[doc = "Register `GPIO8` reader"]
-pub type R = crate::R<GPIO8_SPEC>;
-#[doc = "Register `GPIO8` writer"]
-pub type W = crate::W<GPIO8_SPEC>;
+#[doc = "Register `GPIO%s` reader"]
+pub type R = crate::R<GPIO_SPEC>;
+#[doc = "Register `GPIO%s` writer"]
+pub type W = crate::W<GPIO_SPEC>;
 #[doc = "Field `MCU_OE` reader - Output enable of the pin in sleep mode. 1: Output enabled. 0: Output disabled."]
 pub type MCU_OE_R = crate::BitReader;
 #[doc = "Field `MCU_OE` writer - Output enable of the pin in sleep mode. 1: Output enabled. 0: Output disabled."]
@@ -106,7 +106,7 @@ impl R {
 #[cfg(feature = "impl-register-debug")]
 impl core::fmt::Debug for R {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-        f.debug_struct("GPIO8")
+        f.debug_struct("GPIO")
             .field("mcu_oe", &self.mcu_oe())
             .field("slp_sel", &self.slp_sel())
             .field("mcu_wpd", &self.mcu_wpd())
@@ -124,74 +124,74 @@ impl core::fmt::Debug for R {
 impl W {
     #[doc = "Bit 0 - Output enable of the pin in sleep mode. 1: Output enabled. 0: Output disabled."]
     #[inline(always)]
-    pub fn mcu_oe(&mut self) -> MCU_OE_W<GPIO8_SPEC> {
+    pub fn mcu_oe(&mut self) -> MCU_OE_W<GPIO_SPEC> {
         MCU_OE_W::new(self, 0)
     }
     #[doc = "Bit 1 - Sleep mode selection of this pin. Set to 1 to put the pin in sleep mode."]
     #[inline(always)]
-    pub fn slp_sel(&mut self) -> SLP_SEL_W<GPIO8_SPEC> {
+    pub fn slp_sel(&mut self) -> SLP_SEL_W<GPIO_SPEC> {
         SLP_SEL_W::new(self, 1)
     }
     #[doc = "Bit 2 - Pull-down enable of the pin during sleep mode. 1: Internal pull-down enabled. 0: internal pull-down disabled."]
     #[inline(always)]
-    pub fn mcu_wpd(&mut self) -> MCU_WPD_W<GPIO8_SPEC> {
+    pub fn mcu_wpd(&mut self) -> MCU_WPD_W<GPIO_SPEC> {
         MCU_WPD_W::new(self, 2)
     }
     #[doc = "Bit 3 - Pull-up enable of the pin during sleep mode. 1: Internal pull-up enabled. 0: Internal pull-up disabled."]
     #[inline(always)]
-    pub fn mcu_wpu(&mut self) -> MCU_WPU_W<GPIO8_SPEC> {
+    pub fn mcu_wpu(&mut self) -> MCU_WPU_W<GPIO_SPEC> {
         MCU_WPU_W::new(self, 3)
     }
     #[doc = "Bit 4 - Input enable of the pin during sleep mode. 1: Input enabled. 0: Input disabled."]
     #[inline(always)]
-    pub fn mcu_ie(&mut self) -> MCU_IE_W<GPIO8_SPEC> {
+    pub fn mcu_ie(&mut self) -> MCU_IE_W<GPIO_SPEC> {
         MCU_IE_W::new(self, 4)
     }
     #[doc = "Bit 7 - Pull-down enable of the pin. 1: Internal pull-down enabled. 0: internal pull-down disabled."]
     #[inline(always)]
-    pub fn fun_wpd(&mut self) -> FUN_WPD_W<GPIO8_SPEC> {
+    pub fn fun_wpd(&mut self) -> FUN_WPD_W<GPIO_SPEC> {
         FUN_WPD_W::new(self, 7)
     }
     #[doc = "Bit 8 - Pull-up enable of the pin. 1: Internal pull-up enabled. 0: Internal pull-up disabled."]
     #[inline(always)]
-    pub fn fun_wpu(&mut self) -> FUN_WPU_W<GPIO8_SPEC> {
+    pub fn fun_wpu(&mut self) -> FUN_WPU_W<GPIO_SPEC> {
         FUN_WPU_W::new(self, 8)
     }
     #[doc = "Bit 9 - Input enable of the pin. 1: Input enabled. 0: Input disabled."]
     #[inline(always)]
-    pub fn fun_ie(&mut self) -> FUN_IE_W<GPIO8_SPEC> {
+    pub fn fun_ie(&mut self) -> FUN_IE_W<GPIO_SPEC> {
         FUN_IE_W::new(self, 9)
     }
     #[doc = "Bits 10:11 - Select the drive strength of the pin. 0: ~5 mA. 1: ~10 mA. 2: ~20 mA. 3: ~40 mA."]
     #[inline(always)]
-    pub fn fun_drv(&mut self) -> FUN_DRV_W<GPIO8_SPEC> {
+    pub fn fun_drv(&mut self) -> FUN_DRV_W<GPIO_SPEC> {
         FUN_DRV_W::new(self, 10)
     }
     #[doc = "Bits 12:14 - Select IO MUX function for this signal. 0: Select Function 1. 1: Select Function 2, etc."]
     #[inline(always)]
-    pub fn mcu_sel(&mut self) -> MCU_SEL_W<GPIO8_SPEC> {
+    pub fn mcu_sel(&mut self) -> MCU_SEL_W<GPIO_SPEC> {
         MCU_SEL_W::new(self, 12)
     }
     #[doc = "Bit 15 - Enable filter for pin input signals. 1: Filter enabled. 2: Filter disabled."]
     #[inline(always)]
-    pub fn filter_en(&mut self) -> FILTER_EN_W<GPIO8_SPEC> {
+    pub fn filter_en(&mut self) -> FILTER_EN_W<GPIO_SPEC> {
         FILTER_EN_W::new(self, 15)
     }
 }
-#[doc = "Configuration register for pin GPIO8\n\nYou can [`read`](crate::Reg::read) this register and get [`gpio8::R`](R). You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`gpio8::W`](W). You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
-pub struct GPIO8_SPEC;
-impl crate::RegisterSpec for GPIO8_SPEC {
+#[doc = "Configuration register for pin GPIO%s\n\nYou can [`read`](crate::Reg::read) this register and get [`gpio::R`](R). You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`gpio::W`](W). You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
+pub struct GPIO_SPEC;
+impl crate::RegisterSpec for GPIO_SPEC {
     type Ux = u32;
 }
-#[doc = "`read()` method returns [`gpio8::R`](R) reader structure"]
-impl crate::Readable for GPIO8_SPEC {}
-#[doc = "`write(|w| ..)` method takes [`gpio8::W`](W) writer structure"]
-impl crate::Writable for GPIO8_SPEC {
+#[doc = "`read()` method returns [`gpio::R`](R) reader structure"]
+impl crate::Readable for GPIO_SPEC {}
+#[doc = "`write(|w| ..)` method takes [`gpio::W`](W) writer structure"]
+impl crate::Writable for GPIO_SPEC {
     type Safety = crate::Unsafe;
     const ZERO_TO_MODIFY_FIELDS_BITMAP: u32 = 0;
     const ONE_TO_MODIFY_FIELDS_BITMAP: u32 = 0;
 }
-#[doc = "`reset()` method sets GPIO8 to value 0x0b00"]
-impl crate::Resettable for GPIO8_SPEC {
+#[doc = "`reset()` method sets GPIO%s to value 0x0b00"]
+impl crate::Resettable for GPIO_SPEC {
     const RESET_VALUE: u32 = 0x0b00;
 }
