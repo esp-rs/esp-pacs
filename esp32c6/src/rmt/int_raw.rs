65bd5a26c81d5d37204e07fcb0409da7be2443fd
Kirill Mikhailov <62840029+playfulFence@users.noreply.github.com>
2025-07-03 10:41:57 +0200

    Add upstream patches (C6) (#348)
    * ugh... svdtools....
    
    * finally...

--- esp32c6/src/rmt/int_raw.rs
+++ esp32c6/src/rmt/int_raw.rs
@@ -6,25 +6,25 @@ pub type W = crate::W<INT_RAW_SPEC>;
 pub type CH_TX_END_R = crate::BitReader;
 #[doc = "Field `CH_TX_END(0-1)` writer - The interrupt raw bit for CHANNEL%s. Triggered when transmission done."]
 pub type CH_TX_END_W<'a, REG> = crate::BitWriter<'a, REG>;
-#[doc = "Field `CH_RX_END(2-3)` reader - The interrupt raw bit for CHANNEL2. Triggered when reception done."]
+#[doc = "Field `CH_RX_END(2-3)` reader - The interrupt raw bit for CHANNEL%s. Triggered when reception done."]
 pub type CH_RX_END_R = crate::BitReader;
-#[doc = "Field `CH_RX_END(2-3)` writer - The interrupt raw bit for CHANNEL2. Triggered when reception done."]
+#[doc = "Field `CH_RX_END(2-3)` writer - The interrupt raw bit for CHANNEL%s. Triggered when reception done."]
 pub type CH_RX_END_W<'a, REG> = crate::BitWriter<'a, REG>;
-#[doc = "Field `CH_TX_ERR(0-1)` reader - The interrupt raw bit for CHANNEL4. Triggered when error occurs."]
+#[doc = "Field `CH_TX_ERR(0-1)` reader - todo"]
 pub type CH_TX_ERR_R = crate::BitReader;
-#[doc = "Field `CH_TX_ERR(0-1)` writer - The interrupt raw bit for CHANNEL4. Triggered when error occurs."]
+#[doc = "Field `CH_TX_ERR(0-1)` writer - todo"]
 pub type CH_TX_ERR_W<'a, REG> = crate::BitWriter<'a, REG>;
-#[doc = "Field `CH_RX_ERR(2-3)` reader - The interrupt raw bit for CHANNEL6. Triggered when error occurs."]
+#[doc = "Field `CH_RX_ERR(2-3)` reader - todo"]
 pub type CH_RX_ERR_R = crate::BitReader;
-#[doc = "Field `CH_RX_ERR(2-3)` writer - The interrupt raw bit for CHANNEL6. Triggered when error occurs."]
+#[doc = "Field `CH_RX_ERR(2-3)` writer - todo"]
 pub type CH_RX_ERR_W<'a, REG> = crate::BitWriter<'a, REG>;
 #[doc = "Field `CH_TX_THR_EVENT(0-1)` reader - The interrupt raw bit for CHANNEL%s. Triggered when transmitter sent more data than configured value."]
 pub type CH_TX_THR_EVENT_R = crate::BitReader;
 #[doc = "Field `CH_TX_THR_EVENT(0-1)` writer - The interrupt raw bit for CHANNEL%s. Triggered when transmitter sent more data than configured value."]
 pub type CH_TX_THR_EVENT_W<'a, REG> = crate::BitWriter<'a, REG>;
-#[doc = "Field `CH_RX_THR_EVENT(2-3)` reader - The interrupt raw bit for CHANNEL2. Triggered when receiver receive more data than configured value."]
+#[doc = "Field `CH_RX_THR_EVENT(2-3)` reader - The interrupt raw bit for CHANNEL%s. Triggered when receiver receive more data than configured value."]
 pub type CH_RX_THR_EVENT_R = crate::BitReader;
-#[doc = "Field `CH_RX_THR_EVENT(2-3)` writer - The interrupt raw bit for CHANNEL2. Triggered when receiver receive more data than configured value."]
+#[doc = "Field `CH_RX_THR_EVENT(2-3)` writer - The interrupt raw bit for CHANNEL%s. Triggered when receiver receive more data than configured value."]
 pub type CH_RX_THR_EVENT_W<'a, REG> = crate::BitWriter<'a, REG>;
 #[doc = "Field `CH_TX_LOOP(0-1)` reader - The interrupt raw bit for CHANNEL%s. Triggered when the loop count reaches the configured threshold value."]
 pub type CH_TX_LOOP_R = crate::BitReader;
@@ -56,7 +56,7 @@ impl R {
     pub fn ch1_tx_end(&self) -> CH_TX_END_R {
         CH_TX_END_R::new(((self.bits >> 1) & 1) != 0)
     }
-    #[doc = "The interrupt raw bit for CHANNEL2. Triggered when reception done."]
+    #[doc = "The interrupt raw bit for CHANNEL(2-3). Triggered when reception done."]
     #[doc = ""]
     #[doc = "<div class=\"warning\">`n` is number of field in register. `n == 0` corresponds to `CH2_RX_END` field.</div>"]
     #[inline(always)]
@@ -66,7 +66,7 @@ impl R {
         CH_RX_END_R::new(((self.bits >> (n + 2)) & 1) != 0)
     }
     #[doc = "Iterator for array of:"]
-    #[doc = "The interrupt raw bit for CHANNEL2. Triggered when reception done."]
+    #[doc = "The interrupt raw bit for CHANNEL(2-3). Triggered when reception done."]
     #[inline(always)]
     pub fn ch_rx_end_iter(&self) -> impl Iterator<Item = CH_RX_END_R> + '_ {
         (0..2).map(move |n| CH_RX_END_R::new(((self.bits >> (n + 2)) & 1) != 0))
@@ -76,12 +76,12 @@ impl R {
     pub fn ch2_rx_end(&self) -> CH_RX_END_R {
         CH_RX_END_R::new(((self.bits >> 2) & 1) != 0)
     }
-    #[doc = "Bit 3 - The interrupt raw bit for CHANNEL2. Triggered when reception done."]
+    #[doc = "Bit 3 - The interrupt raw bit for CHANNEL3. Triggered when reception done."]
     #[inline(always)]
     pub fn ch3_rx_end(&self) -> CH_RX_END_R {
         CH_RX_END_R::new(((self.bits >> 3) & 1) != 0)
     }
-    #[doc = "The interrupt raw bit for CHANNEL4. Triggered when error occurs."]
+    #[doc = "todo"]
     #[doc = ""]
     #[doc = "<div class=\"warning\">`n` is number of field in register. `n == 0` corresponds to `CH0_TX_ERR` field.</div>"]
     #[inline(always)]
@@ -91,22 +91,22 @@ impl R {
         CH_TX_ERR_R::new(((self.bits >> (n + 4)) & 1) != 0)
     }
     #[doc = "Iterator for array of:"]
-    #[doc = "The interrupt raw bit for CHANNEL4. Triggered when error occurs."]
+    #[doc = "todo"]
     #[inline(always)]
     pub fn ch_tx_err_iter(&self) -> impl Iterator<Item = CH_TX_ERR_R> + '_ {
         (0..2).map(move |n| CH_TX_ERR_R::new(((self.bits >> (n + 4)) & 1) != 0))
     }
-    #[doc = "Bit 4 - The interrupt raw bit for CHANNEL4. Triggered when error occurs."]
+    #[doc = "Bit 4 - todo"]
     #[inline(always)]
     pub fn ch0_tx_err(&self) -> CH_TX_ERR_R {
         CH_TX_ERR_R::new(((self.bits >> 4) & 1) != 0)
     }
-    #[doc = "Bit 5 - The interrupt raw bit for CHANNEL4. Triggered when error occurs."]
+    #[doc = "Bit 5 - todo"]
     #[inline(always)]
     pub fn ch1_tx_err(&self) -> CH_TX_ERR_R {
         CH_TX_ERR_R::new(((self.bits >> 5) & 1) != 0)
     }
-    #[doc = "The interrupt raw bit for CHANNEL6. Triggered when error occurs."]
+    #[doc = "todo"]
     #[doc = ""]
     #[doc = "<div class=\"warning\">`n` is number of field in register. `n == 0` corresponds to `CH2_RX_ERR` field.</div>"]
     #[inline(always)]
@@ -116,17 +116,17 @@ impl R {
         CH_RX_ERR_R::new(((self.bits >> (n + 6)) & 1) != 0)
     }
     #[doc = "Iterator for array of:"]
-    #[doc = "The interrupt raw bit for CHANNEL6. Triggered when error occurs."]
+    #[doc = "todo"]
     #[inline(always)]
     pub fn ch_rx_err_iter(&self) -> impl Iterator<Item = CH_RX_ERR_R> + '_ {
         (0..2).map(move |n| CH_RX_ERR_R::new(((self.bits >> (n + 6)) & 1) != 0))
     }
-    #[doc = "Bit 6 - The interrupt raw bit for CHANNEL6. Triggered when error occurs."]
+    #[doc = "Bit 6 - todo"]
     #[inline(always)]
     pub fn ch2_rx_err(&self) -> CH_RX_ERR_R {
         CH_RX_ERR_R::new(((self.bits >> 6) & 1) != 0)
     }
-    #[doc = "Bit 7 - The interrupt raw bit for CHANNEL6. Triggered when error occurs."]
+    #[doc = "Bit 7 - todo"]
     #[inline(always)]
     pub fn ch3_rx_err(&self) -> CH_RX_ERR_R {
         CH_RX_ERR_R::new(((self.bits >> 7) & 1) != 0)
@@ -156,7 +156,7 @@ impl R {
     pub fn ch1_tx_thr_event(&self) -> CH_TX_THR_EVENT_R {
         CH_TX_THR_EVENT_R::new(((self.bits >> 9) & 1) != 0)
     }
-    #[doc = "The interrupt raw bit for CHANNEL2. Triggered when receiver receive more data than configured value."]
+    #[doc = "The interrupt raw bit for CHANNEL(2-3). Triggered when receiver receive more data than configured value."]
     #[doc = ""]
     #[doc = "<div class=\"warning\">`n` is number of field in register. `n == 0` corresponds to `CH2_RX_THR_EVENT` field.</div>"]
     #[inline(always)]
@@ -166,7 +166,7 @@ impl R {
         CH_RX_THR_EVENT_R::new(((self.bits >> (n + 10)) & 1) != 0)
     }
     #[doc = "Iterator for array of:"]
-    #[doc = "The interrupt raw bit for CHANNEL2. Triggered when receiver receive more data than configured value."]
+    #[doc = "The interrupt raw bit for CHANNEL(2-3). Triggered when receiver receive more data than configured value."]
     #[inline(always)]
     pub fn ch_rx_thr_event_iter(&self) -> impl Iterator<Item = CH_RX_THR_EVENT_R> + '_ {
         (0..2).map(move |n| CH_RX_THR_EVENT_R::new(((self.bits >> (n + 10)) & 1) != 0))
@@ -176,7 +176,7 @@ impl R {
     pub fn ch2_rx_thr_event(&self) -> CH_RX_THR_EVENT_R {
         CH_RX_THR_EVENT_R::new(((self.bits >> 10) & 1) != 0)
     }
-    #[doc = "Bit 11 - The interrupt raw bit for CHANNEL2. Triggered when receiver receive more data than configured value."]
+    #[doc = "Bit 11 - The interrupt raw bit for CHANNEL3. Triggered when receiver receive more data than configured value."]
     #[inline(always)]
     pub fn ch3_rx_thr_event(&self) -> CH_RX_THR_EVENT_R {
         CH_RX_THR_EVENT_R::new(((self.bits >> 11) & 1) != 0)
@@ -248,7 +248,7 @@ impl W {
     pub fn ch1_tx_end(&mut self) -> CH_TX_END_W<INT_RAW_SPEC> {
         CH_TX_END_W::new(self, 1)
     }
-    #[doc = "The interrupt raw bit for CHANNEL2. Triggered when reception done."]
+    #[doc = "The interrupt raw bit for CHANNEL(2-3). Triggered when reception done."]
     #[doc = ""]
     #[doc = "<div class=\"warning\">`n` is number of field in register. `n == 0` corresponds to `CH2_RX_END` field.</div>"]
     #[inline(always)]
@@ -262,12 +262,12 @@ impl W {
     pub fn ch2_rx_end(&mut self) -> CH_RX_END_W<INT_RAW_SPEC> {
         CH_RX_END_W::new(self, 2)
     }
-    #[doc = "Bit 3 - The interrupt raw bit for CHANNEL2. Triggered when reception done."]
+    #[doc = "Bit 3 - The interrupt raw bit for CHANNEL3. Triggered when reception done."]
     #[inline(always)]
     pub fn ch3_rx_end(&mut self) -> CH_RX_END_W<INT_RAW_SPEC> {
         CH_RX_END_W::new(self, 3)
     }
-    #[doc = "The interrupt raw bit for CHANNEL4. Triggered when error occurs."]
+    #[doc = "todo"]
     #[doc = ""]
     #[doc = "<div class=\"warning\">`n` is number of field in register. `n == 0` corresponds to `CH0_TX_ERR` field.</div>"]
     #[inline(always)]
@@ -276,17 +276,17 @@ impl W {
         [(); 2][n as usize];
         CH_TX_ERR_W::new(self, n + 4)
     }
-    #[doc = "Bit 4 - The interrupt raw bit for CHANNEL4. Triggered when error occurs."]
+    #[doc = "Bit 4 - todo"]
     #[inline(always)]
     pub fn ch0_tx_err(&mut self) -> CH_TX_ERR_W<INT_RAW_SPEC> {
         CH_TX_ERR_W::new(self, 4)
     }
-    #[doc = "Bit 5 - The interrupt raw bit for CHANNEL4. Triggered when error occurs."]
+    #[doc = "Bit 5 - todo"]
     #[inline(always)]
     pub fn ch1_tx_err(&mut self) -> CH_TX_ERR_W<INT_RAW_SPEC> {
         CH_TX_ERR_W::new(self, 5)
     }
-    #[doc = "The interrupt raw bit for CHANNEL6. Triggered when error occurs."]
+    #[doc = "todo"]
     #[doc = ""]
     #[doc = "<div class=\"warning\">`n` is number of field in register. `n == 0` corresponds to `CH2_RX_ERR` field.</div>"]
     #[inline(always)]
@@ -295,12 +295,12 @@ impl W {
         [(); 2][n as usize];
         CH_RX_ERR_W::new(self, n + 6)
     }
-    #[doc = "Bit 6 - The interrupt raw bit for CHANNEL6. Triggered when error occurs."]
+    #[doc = "Bit 6 - todo"]
     #[inline(always)]
     pub fn ch2_rx_err(&mut self) -> CH_RX_ERR_W<INT_RAW_SPEC> {
         CH_RX_ERR_W::new(self, 6)
     }
-    #[doc = "Bit 7 - The interrupt raw bit for CHANNEL6. Triggered when error occurs."]
+    #[doc = "Bit 7 - todo"]
     #[inline(always)]
     pub fn ch3_rx_err(&mut self) -> CH_RX_ERR_W<INT_RAW_SPEC> {
         CH_RX_ERR_W::new(self, 7)
@@ -324,7 +324,7 @@ impl W {
     pub fn ch1_tx_thr_event(&mut self) -> CH_TX_THR_EVENT_W<INT_RAW_SPEC> {
         CH_TX_THR_EVENT_W::new(self, 9)
     }
-    #[doc = "The interrupt raw bit for CHANNEL2. Triggered when receiver receive more data than configured value."]
+    #[doc = "The interrupt raw bit for CHANNEL(2-3). Triggered when receiver receive more data than configured value."]
     #[doc = ""]
     #[doc = "<div class=\"warning\">`n` is number of field in register. `n == 0` corresponds to `CH2_RX_THR_EVENT` field.</div>"]
     #[inline(always)]
@@ -338,7 +338,7 @@ impl W {
     pub fn ch2_rx_thr_event(&mut self) -> CH_RX_THR_EVENT_W<INT_RAW_SPEC> {
         CH_RX_THR_EVENT_W::new(self, 10)
     }
-    #[doc = "Bit 11 - The interrupt raw bit for CHANNEL2. Triggered when receiver receive more data than configured value."]
+    #[doc = "Bit 11 - The interrupt raw bit for CHANNEL3. Triggered when receiver receive more data than configured value."]
     #[inline(always)]
     pub fn ch3_rx_thr_event(&mut self) -> CH_RX_THR_EVENT_W<INT_RAW_SPEC> {
         CH_RX_THR_EVENT_W::new(self, 11)
--- esp32c6/src/usb_device/test.rs
+++ esp32c6/src/usb_device/test.rs
@@ -2,99 +2,99 @@
 pub type R = crate::R<TEST_SPEC>;
 #[doc = "Register `TEST` writer"]
 pub type W = crate::W<TEST_SPEC>;
-#[doc = "Field `TEST_ENABLE` reader - Enable test of the USB pad"]
-pub type TEST_ENABLE_R = crate::BitReader;
-#[doc = "Field `TEST_ENABLE` writer - Enable test of the USB pad"]
-pub type TEST_ENABLE_W<'a, REG> = crate::BitWriter<'a, REG>;
-#[doc = "Field `TEST_USB_OE` reader - USB pad oen in test"]
-pub type TEST_USB_OE_R = crate::BitReader;
-#[doc = "Field `TEST_USB_OE` writer - USB pad oen in test"]
-pub type TEST_USB_OE_W<'a, REG> = crate::BitWriter<'a, REG>;
-#[doc = "Field `TEST_TX_DP` reader - USB D+ tx value in test"]
-pub type TEST_TX_DP_R = crate::BitReader;
-#[doc = "Field `TEST_TX_DP` writer - USB D+ tx value in test"]
-pub type TEST_TX_DP_W<'a, REG> = crate::BitWriter<'a, REG>;
-#[doc = "Field `TEST_TX_DM` reader - USB D- tx value in test"]
-pub type TEST_TX_DM_R = crate::BitReader;
-#[doc = "Field `TEST_TX_DM` writer - USB D- tx value in test"]
-pub type TEST_TX_DM_W<'a, REG> = crate::BitWriter<'a, REG>;
-#[doc = "Field `TEST_RX_RCV` reader - USB RCV value in test"]
-pub type TEST_RX_RCV_R = crate::BitReader;
-#[doc = "Field `TEST_RX_DP` reader - USB D+ rx value in test"]
-pub type TEST_RX_DP_R = crate::BitReader;
-#[doc = "Field `TEST_RX_DM` reader - USB D- rx value in test"]
-pub type TEST_RX_DM_R = crate::BitReader;
+#[doc = "Field `ENABLE` reader - Enable test of the USB pad"]
+pub type ENABLE_R = crate::BitReader;
+#[doc = "Field `ENABLE` writer - Enable test of the USB pad"]
+pub type ENABLE_W<'a, REG> = crate::BitWriter<'a, REG>;
+#[doc = "Field `USB_OE` reader - USB pad oen in test"]
+pub type USB_OE_R = crate::BitReader;
+#[doc = "Field `USB_OE` writer - USB pad oen in test"]
+pub type USB_OE_W<'a, REG> = crate::BitWriter<'a, REG>;
+#[doc = "Field `TX_DP` reader - USB D+ tx value in test"]
+pub type TX_DP_R = crate::BitReader;
+#[doc = "Field `TX_DP` writer - USB D+ tx value in test"]
+pub type TX_DP_W<'a, REG> = crate::BitWriter<'a, REG>;
+#[doc = "Field `TX_DM` reader - USB D- tx value in test"]
+pub type TX_DM_R = crate::BitReader;
+#[doc = "Field `TX_DM` writer - USB D- tx value in test"]
+pub type TX_DM_W<'a, REG> = crate::BitWriter<'a, REG>;
+#[doc = "Field `RX_RCV` reader - USB RCV value in test"]
+pub type RX_RCV_R = crate::BitReader;
+#[doc = "Field `RX_DP` reader - USB D+ rx value in test"]
+pub type RX_DP_R = crate::BitReader;
+#[doc = "Field `RX_DM` reader - USB D- rx value in test"]
+pub type RX_DM_R = crate::BitReader;
 impl R {
     #[doc = "Bit 0 - Enable test of the USB pad"]
     #[inline(always)]
-    pub fn test_enable(&self) -> TEST_ENABLE_R {
-        TEST_ENABLE_R::new((self.bits & 1) != 0)
+    pub fn enable(&self) -> ENABLE_R {
+        ENABLE_R::new((self.bits & 1) != 0)
     }
     #[doc = "Bit 1 - USB pad oen in test"]
     #[inline(always)]
-    pub fn test_usb_oe(&self) -> TEST_USB_OE_R {
-        TEST_USB_OE_R::new(((self.bits >> 1) & 1) != 0)
+    pub fn usb_oe(&self) -> USB_OE_R {
+        USB_OE_R::new(((self.bits >> 1) & 1) != 0)
     }
     #[doc = "Bit 2 - USB D+ tx value in test"]
     #[inline(always)]
-    pub fn test_tx_dp(&self) -> TEST_TX_DP_R {
-        TEST_TX_DP_R::new(((self.bits >> 2) & 1) != 0)
+    pub fn tx_dp(&self) -> TX_DP_R {
+        TX_DP_R::new(((self.bits >> 2) & 1) != 0)
     }
     #[doc = "Bit 3 - USB D- tx value in test"]
     #[inline(always)]
-    pub fn test_tx_dm(&self) -> TEST_TX_DM_R {
-        TEST_TX_DM_R::new(((self.bits >> 3) & 1) != 0)
+    pub fn tx_dm(&self) -> TX_DM_R {
+        TX_DM_R::new(((self.bits >> 3) & 1) != 0)
     }
     #[doc = "Bit 4 - USB RCV value in test"]
     #[inline(always)]
-    pub fn test_rx_rcv(&self) -> TEST_RX_RCV_R {
-        TEST_RX_RCV_R::new(((self.bits >> 4) & 1) != 0)
+    pub fn rx_rcv(&self) -> RX_RCV_R {
+        RX_RCV_R::new(((self.bits >> 4) & 1) != 0)
     }
     #[doc = "Bit 5 - USB D+ rx value in test"]
     #[inline(always)]
-    pub fn test_rx_dp(&self) -> TEST_RX_DP_R {
-        TEST_RX_DP_R::new(((self.bits >> 5) & 1) != 0)
+    pub fn rx_dp(&self) -> RX_DP_R {
+        RX_DP_R::new(((self.bits >> 5) & 1) != 0)
     }
     #[doc = "Bit 6 - USB D- rx value in test"]
     #[inline(always)]
-    pub fn test_rx_dm(&self) -> TEST_RX_DM_R {
-        TEST_RX_DM_R::new(((self.bits >> 6) & 1) != 0)
+    pub fn rx_dm(&self) -> RX_DM_R {
+        RX_DM_R::new(((self.bits >> 6) & 1) != 0)
     }
 }
 #[cfg(feature = "impl-register-debug")]
 impl core::fmt::Debug for R {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
         f.debug_struct("TEST")
-            .field("test_enable", &self.test_enable())
-            .field("test_usb_oe", &self.test_usb_oe())
-            .field("test_tx_dp", &self.test_tx_dp())
-            .field("test_tx_dm", &self.test_tx_dm())
-            .field("test_rx_rcv", &self.test_rx_rcv())
-            .field("test_rx_dp", &self.test_rx_dp())
-            .field("test_rx_dm", &self.test_rx_dm())
+            .field("enable", &self.enable())
+            .field("usb_oe", &self.usb_oe())
+            .field("tx_dp", &self.tx_dp())
+            .field("tx_dm", &self.tx_dm())
+            .field("rx_rcv", &self.rx_rcv())
+            .field("rx_dp", &self.rx_dp())
+            .field("rx_dm", &self.rx_dm())
             .finish()
     }
 }
 impl W {
     #[doc = "Bit 0 - Enable test of the USB pad"]
     #[inline(always)]
-    pub fn test_enable(&mut self) -> TEST_ENABLE_W<TEST_SPEC> {
-        TEST_ENABLE_W::new(self, 0)
+    pub fn enable(&mut self) -> ENABLE_W<TEST_SPEC> {
+        ENABLE_W::new(self, 0)
     }
     #[doc = "Bit 1 - USB pad oen in test"]
     #[inline(always)]
-    pub fn test_usb_oe(&mut self) -> TEST_USB_OE_W<TEST_SPEC> {
-        TEST_USB_OE_W::new(self, 1)
+    pub fn usb_oe(&mut self) -> USB_OE_W<TEST_SPEC> {
+        USB_OE_W::new(self, 1)
     }
     #[doc = "Bit 2 - USB D+ tx value in test"]
     #[inline(always)]
-    pub fn test_tx_dp(&mut self) -> TEST_TX_DP_W<TEST_SPEC> {
-        TEST_TX_DP_W::new(self, 2)
+    pub fn tx_dp(&mut self) -> TX_DP_W<TEST_SPEC> {
+        TX_DP_W::new(self, 2)
     }
     #[doc = "Bit 3 - USB D- tx value in test"]
     #[inline(always)]
-    pub fn test_tx_dm(&mut self) -> TEST_TX_DM_W<TEST_SPEC> {
-        TEST_TX_DM_W::new(self, 3)
+    pub fn tx_dm(&mut self) -> TX_DM_W<TEST_SPEC> {
+        TX_DM_W::new(self, 3)
     }
 }
 #[doc = "Registers used for debugging the PHY.\n\nYou can [`read`](crate::Reg::read) this register and get [`test::R`](R). You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`test::W`](W). You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
