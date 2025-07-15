ba6cdd3dc4383ddb0bc6c4dfca8ef1ef185ab8e7
Dániel Buga <bugadani@gmail.com>
2025-07-15 16:57:27 +0200

    Arrayify PERI_BACKUP registers

--- esp32s3/src/peri_backup.rs
+++ esp32s3/src/peri_backup.rs
@@ -5,15 +5,12 @@ pub struct RegisterBlock {
     config: CONFIG,
     apb_addr: APB_ADDR,
     mem_addr: MEM_ADDR,
-    reg_map0: REG_MAP0,
-    reg_map1: REG_MAP1,
-    reg_map2: REG_MAP2,
-    reg_map3: REG_MAP3,
+    reg_map: [REG_MAP; 4],
     int_raw: INT_RAW,
     int_st: INT_ST,
     int_ena: INT_ENA,
     int_clr: INT_CLR,
-    _reserved11: [u8; 0xd0],
+    _reserved8: [u8; 0xd0],
     date: DATE,
 }
 impl RegisterBlock {
@@ -32,25 +29,16 @@ impl RegisterBlock {
     pub const fn mem_addr(&self) -> &MEM_ADDR {
         &self.mem_addr
     }
-    #[doc = "0x0c - x"]
+    #[doc = "0x0c..0x1c - x"]
     #[inline(always)]
-    pub const fn reg_map0(&self) -> &REG_MAP0 {
-        &self.reg_map0
+    pub const fn reg_map(&self, n: usize) -> &REG_MAP {
+        &self.reg_map[n]
     }
-    #[doc = "0x10 - x"]
+    #[doc = "Iterator for array of:"]
+    #[doc = "0x0c..0x1c - x"]
     #[inline(always)]
-    pub const fn reg_map1(&self) -> &REG_MAP1 {
-        &self.reg_map1
-    }
-    #[doc = "0x14 - x"]
-    #[inline(always)]
-    pub const fn reg_map2(&self) -> &REG_MAP2 {
-        &self.reg_map2
-    }
-    #[doc = "0x18 - x"]
-    #[inline(always)]
-    pub const fn reg_map3(&self) -> &REG_MAP3 {
-        &self.reg_map3
+    pub fn reg_map_iter(&self) -> impl Iterator<Item = &REG_MAP> {
+        self.reg_map.iter()
     }
     #[doc = "0x1c - x"]
     #[inline(always)]
@@ -90,22 +78,10 @@ pub mod apb_addr;
 pub type MEM_ADDR = crate::Reg<mem_addr::MEM_ADDR_SPEC>;
 #[doc = "x"]
 pub mod mem_addr;
-#[doc = "REG_MAP0 (rw) register accessor: x\n\nYou can [`read`](crate::Reg::read) this register and get [`reg_map0::R`]. You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`reg_map0::W`]. You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@reg_map0`] module"]
-pub type REG_MAP0 = crate::Reg<reg_map0::REG_MAP0_SPEC>;
+#[doc = "REG_MAP (rw) register accessor: x\n\nYou can [`read`](crate::Reg::read) this register and get [`reg_map::R`]. You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`reg_map::W`]. You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@reg_map`] module"]
+pub type REG_MAP = crate::Reg<reg_map::REG_MAP_SPEC>;
 #[doc = "x"]
-pub mod reg_map0;
-#[doc = "REG_MAP1 (rw) register accessor: x\n\nYou can [`read`](crate::Reg::read) this register and get [`reg_map1::R`]. You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`reg_map1::W`]. You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@reg_map1`] module"]
-pub type REG_MAP1 = crate::Reg<reg_map1::REG_MAP1_SPEC>;
-#[doc = "x"]
-pub mod reg_map1;
-#[doc = "REG_MAP2 (rw) register accessor: x\n\nYou can [`read`](crate::Reg::read) this register and get [`reg_map2::R`]. You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`reg_map2::W`]. You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@reg_map2`] module"]
-pub type REG_MAP2 = crate::Reg<reg_map2::REG_MAP2_SPEC>;
-#[doc = "x"]
-pub mod reg_map2;
-#[doc = "REG_MAP3 (rw) register accessor: x\n\nYou can [`read`](crate::Reg::read) this register and get [`reg_map3::R`]. You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`reg_map3::W`]. You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@reg_map3`] module"]
-pub type REG_MAP3 = crate::Reg<reg_map3::REG_MAP3_SPEC>;
-#[doc = "x"]
-pub mod reg_map3;
+pub mod reg_map;
 #[doc = "INT_RAW (r) register accessor: x\n\nYou can [`read`](crate::Reg::read) this register and get [`int_raw::R`]. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@int_raw`] module"]
 pub type INT_RAW = crate::Reg<int_raw::INT_RAW_SPEC>;
 #[doc = "x"]
