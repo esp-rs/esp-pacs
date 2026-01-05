5d7a47921f25d96ab3ddd4a16bad31543bc9a8be
Dániel Buga <bugadani@gmail.com>
2026-01-05 12:17:20 +0100

    Rename clock control bits related to assist_debug and dedicated_gpio (#388)
    * Rename fields for consistency
    
    * Generate PAC

--- esp32s3/src/system/cpu_peri_rst_en.rs
+++ esp32s3/src/system/cpu_peri_rst_en.rs
@@ -2,45 +2,45 @@
 pub type R = crate::R<CPU_PERI_RST_EN_SPEC>;
 #[doc = "Register `CPU_PERI_RST_EN` writer"]
 pub type W = crate::W<CPU_PERI_RST_EN_SPEC>;
-#[doc = "Field `RST_EN_ASSIST_DEBUG` reader - Set 1 to let assist_debug module reset"]
-pub type RST_EN_ASSIST_DEBUG_R = crate::BitReader;
-#[doc = "Field `RST_EN_ASSIST_DEBUG` writer - Set 1 to let assist_debug module reset"]
-pub type RST_EN_ASSIST_DEBUG_W<'a, REG> = crate::BitWriter<'a, REG>;
-#[doc = "Field `RST_EN_DEDICATED_GPIO` reader - Set 1 to let dedicated_gpio module reset"]
-pub type RST_EN_DEDICATED_GPIO_R = crate::BitReader;
-#[doc = "Field `RST_EN_DEDICATED_GPIO` writer - Set 1 to let dedicated_gpio module reset"]
-pub type RST_EN_DEDICATED_GPIO_W<'a, REG> = crate::BitWriter<'a, REG>;
+#[doc = "Field `ASSIST_DEBUG_RST` reader - Set 1 to let assist_debug module reset"]
+pub type ASSIST_DEBUG_RST_R = crate::BitReader;
+#[doc = "Field `ASSIST_DEBUG_RST` writer - Set 1 to let assist_debug module reset"]
+pub type ASSIST_DEBUG_RST_W<'a, REG> = crate::BitWriter<'a, REG>;
+#[doc = "Field `DEDICATED_GPIO_RST` reader - Set 1 to let dedicated_gpio module reset"]
+pub type DEDICATED_GPIO_RST_R = crate::BitReader;
+#[doc = "Field `DEDICATED_GPIO_RST` writer - Set 1 to let dedicated_gpio module reset"]
+pub type DEDICATED_GPIO_RST_W<'a, REG> = crate::BitWriter<'a, REG>;
 impl R {
     #[doc = "Bit 6 - Set 1 to let assist_debug module reset"]
     #[inline(always)]
-    pub fn rst_en_assist_debug(&self) -> RST_EN_ASSIST_DEBUG_R {
-        RST_EN_ASSIST_DEBUG_R::new(((self.bits >> 6) & 1) != 0)
+    pub fn assist_debug_rst(&self) -> ASSIST_DEBUG_RST_R {
+        ASSIST_DEBUG_RST_R::new(((self.bits >> 6) & 1) != 0)
     }
     #[doc = "Bit 7 - Set 1 to let dedicated_gpio module reset"]
     #[inline(always)]
-    pub fn rst_en_dedicated_gpio(&self) -> RST_EN_DEDICATED_GPIO_R {
-        RST_EN_DEDICATED_GPIO_R::new(((self.bits >> 7) & 1) != 0)
+    pub fn dedicated_gpio_rst(&self) -> DEDICATED_GPIO_RST_R {
+        DEDICATED_GPIO_RST_R::new(((self.bits >> 7) & 1) != 0)
     }
 }
 #[cfg(feature = "impl-register-debug")]
 impl core::fmt::Debug for R {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
         f.debug_struct("CPU_PERI_RST_EN")
-            .field("rst_en_assist_debug", &self.rst_en_assist_debug())
-            .field("rst_en_dedicated_gpio", &self.rst_en_dedicated_gpio())
+            .field("assist_debug_rst", &self.assist_debug_rst())
+            .field("dedicated_gpio_rst", &self.dedicated_gpio_rst())
             .finish()
     }
 }
 impl W {
     #[doc = "Bit 6 - Set 1 to let assist_debug module reset"]
     #[inline(always)]
-    pub fn rst_en_assist_debug(&mut self) -> RST_EN_ASSIST_DEBUG_W<'_, CPU_PERI_RST_EN_SPEC> {
-        RST_EN_ASSIST_DEBUG_W::new(self, 6)
+    pub fn assist_debug_rst(&mut self) -> ASSIST_DEBUG_RST_W<'_, CPU_PERI_RST_EN_SPEC> {
+        ASSIST_DEBUG_RST_W::new(self, 6)
     }
     #[doc = "Bit 7 - Set 1 to let dedicated_gpio module reset"]
     #[inline(always)]
-    pub fn rst_en_dedicated_gpio(&mut self) -> RST_EN_DEDICATED_GPIO_W<'_, CPU_PERI_RST_EN_SPEC> {
-        RST_EN_DEDICATED_GPIO_W::new(self, 7)
+    pub fn dedicated_gpio_rst(&mut self) -> DEDICATED_GPIO_RST_W<'_, CPU_PERI_RST_EN_SPEC> {
+        DEDICATED_GPIO_RST_W::new(self, 7)
     }
 }
 #[doc = "cpu_peripheral reset configuration regsiter\n\nYou can [`read`](crate::Reg::read) this register and get [`cpu_peri_rst_en::R`](R). You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`cpu_peri_rst_en::W`](W). You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
