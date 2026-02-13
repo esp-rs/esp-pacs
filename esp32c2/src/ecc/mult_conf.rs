8436a1d181b02bdecf3b2e43172a50f28aecb644
Dániel Buga <bugadani@gmail.com>
2026-02-13 14:04:23 +0100

    Backport register field descriptions for C2

--- esp32c2/src/ecc/mult_conf.rs
+++ esp32c2/src/ecc/mult_conf.rs
@@ -2,13 +2,13 @@
 pub type R = crate::R<MULT_CONF_SPEC>;
 #[doc = "Register `MULT_CONF` writer"]
 pub type W = crate::W<MULT_CONF_SPEC>;
-#[doc = "Field `START` reader - Set this bit to reset receiver"]
+#[doc = "Field `START` reader - Configures whether to start calculation of ECC Accelerator."]
 pub type START_R = crate::BitReader;
-#[doc = "Field `START` writer - Set this bit to reset receiver"]
+#[doc = "Field `START` writer - Configures whether to start calculation of ECC Accelerator."]
 pub type START_W<'a, REG> = crate::BitWriter<'a, REG>;
-#[doc = "Field `RESET` writer - Set this bit to reset Rx AFIFO"]
+#[doc = "Field `RESET` writer - Configures whether to reset ECC Accelerator."]
 pub type RESET_W<'a, REG> = crate::BitWriter<'a, REG>;
-#[doc = "Set this bit to start receiving data\n\nValue on reset: 0"]
+#[doc = "Configures the key length mode bit of ECC Accelerator.\n\nValue on reset: 0"]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum KEY_LENGTH {
@@ -23,7 +23,7 @@ impl From<KEY_LENGTH> for bool {
         variant as u8 != 0
     }
 }
-#[doc = "Field `KEY_LENGTH` reader - Set this bit to start receiving data"]
+#[doc = "Field `KEY_LENGTH` reader - Configures the key length mode bit of ECC Accelerator."]
 pub type KEY_LENGTH_R = crate::BitReader<KEY_LENGTH>;
 impl KEY_LENGTH_R {
     #[doc = "Get enumerated values variant"]
@@ -45,7 +45,7 @@ impl KEY_LENGTH_R {
         *self == KEY_LENGTH::P256
     }
 }
-#[doc = "Field `KEY_LENGTH` writer - Set this bit to start receiving data"]
+#[doc = "Field `KEY_LENGTH` writer - Configures the key length mode bit of ECC Accelerator."]
 pub type KEY_LENGTH_W<'a, REG> = crate::BitWriter<'a, REG, KEY_LENGTH>;
 impl<'a, REG> KEY_LENGTH_W<'a, REG>
 where
@@ -62,47 +62,47 @@ where
         self.variant(KEY_LENGTH::P256)
     }
 }
-#[doc = "Field `SECURITY_MODE` reader - Set this bit to enable slave receiver mode"]
+#[doc = "Field `SECURITY_MODE` reader - Configures the security mode of ECC Accelerator."]
 pub type SECURITY_MODE_R = crate::BitReader;
-#[doc = "Field `SECURITY_MODE` writer - Set this bit to enable slave receiver mode"]
+#[doc = "Field `SECURITY_MODE` writer - Configures the security mode of ECC Accelerator."]
 pub type SECURITY_MODE_W<'a, REG> = crate::BitWriter<'a, REG>;
-#[doc = "Field `CLK_EN` reader - clk gate"]
+#[doc = "Field `CLK_EN` reader - Configures whether to force on register clock gate."]
 pub type CLK_EN_R = crate::BitReader;
-#[doc = "Field `CLK_EN` writer - clk gate"]
+#[doc = "Field `CLK_EN` writer - Configures whether to force on register clock gate."]
 pub type CLK_EN_W<'a, REG> = crate::BitWriter<'a, REG>;
-#[doc = "Field `WORK_MODE` reader - Reserved"]
+#[doc = "Field `WORK_MODE` reader - Configures the work mode of ECC Accelerator."]
 pub type WORK_MODE_R = crate::FieldReader;
-#[doc = "Field `WORK_MODE` writer - Reserved"]
+#[doc = "Field `WORK_MODE` writer - Configures the work mode of ECC Accelerator."]
 pub type WORK_MODE_W<'a, REG> = crate::FieldWriter<'a, REG, 3>;
-#[doc = "Field `VERIFICATION_RESULT` reader - Reserve"]
+#[doc = "Field `VERIFICATION_RESULT` reader - Represents the verification result of ECC Accelerator, valid only when calculation is done."]
 pub type VERIFICATION_RESULT_R = crate::BitReader;
 impl R {
-    #[doc = "Bit 0 - Set this bit to reset receiver"]
+    #[doc = "Bit 0 - Configures whether to start calculation of ECC Accelerator."]
     #[inline(always)]
     pub fn start(&self) -> START_R {
         START_R::new((self.bits & 1) != 0)
     }
-    #[doc = "Bit 2 - Set this bit to start receiving data"]
+    #[doc = "Bit 2 - Configures the key length mode bit of ECC Accelerator."]
     #[inline(always)]
     pub fn key_length(&self) -> KEY_LENGTH_R {
         KEY_LENGTH_R::new(((self.bits >> 2) & 1) != 0)
     }
-    #[doc = "Bit 3 - Set this bit to enable slave receiver mode"]
+    #[doc = "Bit 3 - Configures the security mode of ECC Accelerator."]
     #[inline(always)]
     pub fn security_mode(&self) -> SECURITY_MODE_R {
         SECURITY_MODE_R::new(((self.bits >> 3) & 1) != 0)
     }
-    #[doc = "Bit 4 - clk gate"]
+    #[doc = "Bit 4 - Configures whether to force on register clock gate."]
     #[inline(always)]
     pub fn clk_en(&self) -> CLK_EN_R {
         CLK_EN_R::new(((self.bits >> 4) & 1) != 0)
     }
-    #[doc = "Bits 5:7 - Reserved"]
+    #[doc = "Bits 5:7 - Configures the work mode of ECC Accelerator."]
     #[inline(always)]
     pub fn work_mode(&self) -> WORK_MODE_R {
         WORK_MODE_R::new(((self.bits >> 5) & 7) as u8)
     }
-    #[doc = "Bit 8 - Reserve"]
+    #[doc = "Bit 8 - Represents the verification result of ECC Accelerator, valid only when calculation is done."]
     #[inline(always)]
     pub fn verification_result(&self) -> VERIFICATION_RESULT_R {
         VERIFICATION_RESULT_R::new(((self.bits >> 8) & 1) != 0)
@@ -122,32 +122,32 @@ impl core::fmt::Debug for R {
     }
 }
 impl W {
-    #[doc = "Bit 0 - Set this bit to reset receiver"]
+    #[doc = "Bit 0 - Configures whether to start calculation of ECC Accelerator."]
     #[inline(always)]
     pub fn start(&mut self) -> START_W<'_, MULT_CONF_SPEC> {
         START_W::new(self, 0)
     }
-    #[doc = "Bit 1 - Set this bit to reset Rx AFIFO"]
+    #[doc = "Bit 1 - Configures whether to reset ECC Accelerator."]
     #[inline(always)]
     pub fn reset(&mut self) -> RESET_W<'_, MULT_CONF_SPEC> {
         RESET_W::new(self, 1)
     }
-    #[doc = "Bit 2 - Set this bit to start receiving data"]
+    #[doc = "Bit 2 - Configures the key length mode bit of ECC Accelerator."]
     #[inline(always)]
     pub fn key_length(&mut self) -> KEY_LENGTH_W<'_, MULT_CONF_SPEC> {
         KEY_LENGTH_W::new(self, 2)
     }
-    #[doc = "Bit 3 - Set this bit to enable slave receiver mode"]
+    #[doc = "Bit 3 - Configures the security mode of ECC Accelerator."]
     #[inline(always)]
     pub fn security_mode(&mut self) -> SECURITY_MODE_W<'_, MULT_CONF_SPEC> {
         SECURITY_MODE_W::new(self, 3)
     }
-    #[doc = "Bit 4 - clk gate"]
+    #[doc = "Bit 4 - Configures whether to force on register clock gate."]
     #[inline(always)]
     pub fn clk_en(&mut self) -> CLK_EN_W<'_, MULT_CONF_SPEC> {
         CLK_EN_W::new(self, 4)
     }
-    #[doc = "Bits 5:7 - Reserved"]
+    #[doc = "Bits 5:7 - Configures the work mode of ECC Accelerator."]
     #[inline(always)]
     pub fn work_mode(&mut self) -> WORK_MODE_W<'_, MULT_CONF_SPEC> {
         WORK_MODE_W::new(self, 5)
