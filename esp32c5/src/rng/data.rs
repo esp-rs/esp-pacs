d45fbe88ea139589061d35544db27f4f23f8cf54
Dániel Buga <bugadani@gmail.com>
2026-02-13 11:37:37 +0100

    Add RNG peripheral (#399)

---
+++ esp32c5/src/rng/data.rs
@@ -0,0 +1,24 @@
+#[doc = "Register `DATA` reader"]
+pub type R = crate::R<DATA_SPEC>;
+#[doc = "Register `DATA` writer"]
+pub type W = crate::W<DATA_SPEC>;
+#[cfg(feature = "impl-register-debug")]
+impl core::fmt::Debug for R {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "{}", self.bits())
+    }
+}
+impl W {}
+#[doc = "Random number data\n\nYou can [`read`](crate::Reg::read) this register and get [`data::R`](R). You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`data::W`](W). You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
+pub struct DATA_SPEC;
+impl crate::RegisterSpec for DATA_SPEC {
+    type Ux = u32;
+}
+#[doc = "`read()` method returns [`data::R`](R) reader structure"]
+impl crate::Readable for DATA_SPEC {}
+#[doc = "`write(|w| ..)` method takes [`data::W`](W) writer structure"]
+impl crate::Writable for DATA_SPEC {
+    type Safety = crate::Unsafe;
+}
+#[doc = "`reset()` method sets DATA to value 0"]
+impl crate::Resettable for DATA_SPEC {}
