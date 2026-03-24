405f40edcb665ce5dc9e99428ea23670e004ebc8
Björn Quentin <bjoernQ@users.noreply.github.com>
2026-03-24 09:24:45 +0100

    (Manually) add RNG peripheral (#409)

---
+++ esp32c61/src/rng.rs
@@ -0,0 +1,18 @@
+#[repr(C)]
+#[cfg_attr(feature = "impl-register-debug", derive(Debug))]
+#[doc = "Register block"]
+pub struct RegisterBlock {
+    _reserved0: [u8; 0x08],
+    data: DATA,
+}
+impl RegisterBlock {
+    #[doc = "0x08 - Random number data"]
+    #[inline(always)]
+    pub const fn data(&self) -> &DATA {
+        &self.data
+    }
+}
+#[doc = "DATA (rw) register accessor: Random number data\n\nYou can [`read`](crate::Reg::read) this register and get [`data::R`]. You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`data::W`]. You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@data`] module"]
+pub type DATA = crate::Reg<data::DATA_SPEC>;
+#[doc = "Random number data"]
+pub mod data;
