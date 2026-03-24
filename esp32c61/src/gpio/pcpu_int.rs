663c742c6e5b51df96253594661bca866f758af8
Kirill Mikhailov <62840029+playfulFence@users.noreply.github.com>
2026-03-24 15:27:55 +0100

    C61: GPIO fixes (#410)

---
+++ esp32c61/src/gpio/pcpu_int.rs
@@ -0,0 +1,28 @@
+#[doc = "Register `PCPU_INT` reader"]
+pub type R = crate::R<PCPU_INT_SPEC>;
+#[doc = "Field `PROCPU_INT` reader - GPIO_PROCPU_INT interrupt status register for GPIO0-31"]
+pub type PROCPU_INT_R = crate::FieldReader<u32>;
+impl R {
+    #[doc = "Bits 0:31 - GPIO_PROCPU_INT interrupt status register for GPIO0-31"]
+    #[inline(always)]
+    pub fn procpu_int(&self) -> PROCPU_INT_R {
+        PROCPU_INT_R::new(self.bits)
+    }
+}
+#[cfg(feature = "impl-register-debug")]
+impl core::fmt::Debug for R {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        f.debug_struct("PCPU_INT")
+            .field("procpu_int", &self.procpu_int())
+            .finish()
+    }
+}
+#[doc = "GPIO_PROCPU_INT interrupt status register for GPIO0-31\n\nYou can [`read`](crate::Reg::read) this register and get [`pcpu_int::R`](R). See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
+pub struct PCPU_INT_SPEC;
+impl crate::RegisterSpec for PCPU_INT_SPEC {
+    type Ux = u32;
+}
+#[doc = "`read()` method returns [`pcpu_int::R`](R) reader structure"]
+impl crate::Readable for PCPU_INT_SPEC {}
+#[doc = "`reset()` method sets PCPU_INT to value 0"]
+impl crate::Resettable for PCPU_INT_SPEC {}
