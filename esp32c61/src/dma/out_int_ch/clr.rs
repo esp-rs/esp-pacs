e88134e8dbef49ee7d764bbd8051638047ecd31c
Dániel Buga <bugadani@gmail.com>
2026-04-14 10:26:03 +0200

    Re-generate PAC

--- esp32c61/src/dma/out_int_ch/clr.rs
+++ esp32c61/src/dma/out_int_ch/clr.rs
@@ -1,19 +1,19 @@
 #[doc = "Register `CLR` writer"]
 pub type W = crate::W<CLR_SPEC>;
-#[doc = "Field `OUT_DONE_CH0_INT_CLR` writer - Write 1 to clear AHB_DMA_OUT_DONE_CH0_INT"]
-pub type OUT_DONE_CH0_INT_CLR_W<'a, REG> = crate::BitWriter1C<'a, REG>;
-#[doc = "Field `OUT_EOF_CH0_INT_CLR` writer - Write 1 to clear AHB_DMA_OUT_EOF_CH0_INT"]
-pub type OUT_EOF_CH0_INT_CLR_W<'a, REG> = crate::BitWriter1C<'a, REG>;
-#[doc = "Field `OUT_DSCR_ERR_CH0_INT_CLR` writer - Write 1 to clear AHB_DMA_OUT_DSCR_ERR_CH0_INT"]
-pub type OUT_DSCR_ERR_CH0_INT_CLR_W<'a, REG> = crate::BitWriter1C<'a, REG>;
-#[doc = "Field `OUT_TOTAL_EOF_CH0_INT_CLR` writer - Write 1 to clear AHB_DMA_OUT_TOTAL_EOF_CH0_INT"]
-pub type OUT_TOTAL_EOF_CH0_INT_CLR_W<'a, REG> = crate::BitWriter1C<'a, REG>;
-#[doc = "Field `OUTFIFO_OVF_CH0_INT_CLR` writer - Write 1 to clear AHB_DMA_OUTFIFO_OVF_CH0_INT"]
-pub type OUTFIFO_OVF_CH0_INT_CLR_W<'a, REG> = crate::BitWriter1C<'a, REG>;
-#[doc = "Field `OUTFIFO_UDF_CH0_INT_CLR` writer - Write 1 to clear AHB_DMA_OUTFIFO_UDF_CH0_INT"]
-pub type OUTFIFO_UDF_CH0_INT_CLR_W<'a, REG> = crate::BitWriter1C<'a, REG>;
-#[doc = "Field `OUT_AHBINF_RESP_ERR_CH0_INT_CLR` writer - Write 1 to clear AHB_DMA_OUT_RESP_ERR_CH0_INT"]
-pub type OUT_AHBINF_RESP_ERR_CH0_INT_CLR_W<'a, REG> = crate::BitWriter1C<'a, REG>;
+#[doc = "Field `OUT_DONE` writer - Write 1 to clear AHB_DMA_OUT_DONE_CH0_INT"]
+pub type OUT_DONE_W<'a, REG> = crate::BitWriter1C<'a, REG>;
+#[doc = "Field `OUT_EOF` writer - Write 1 to clear AHB_DMA_OUT_EOF_CH0_INT"]
+pub type OUT_EOF_W<'a, REG> = crate::BitWriter1C<'a, REG>;
+#[doc = "Field `OUT_DSCR_ERR` writer - Write 1 to clear AHB_DMA_OUT_DSCR_ERR_CH0_INT"]
+pub type OUT_DSCR_ERR_W<'a, REG> = crate::BitWriter1C<'a, REG>;
+#[doc = "Field `OUT_TOTAL_EOF` writer - Write 1 to clear AHB_DMA_OUT_TOTAL_EOF_CH0_INT"]
+pub type OUT_TOTAL_EOF_W<'a, REG> = crate::BitWriter1C<'a, REG>;
+#[doc = "Field `OUTFIFO_OVF` writer - Write 1 to clear AHB_DMA_OUTFIFO_OVF_CH0_INT"]
+pub type OUTFIFO_OVF_W<'a, REG> = crate::BitWriter1C<'a, REG>;
+#[doc = "Field `OUTFIFO_UDF` writer - Write 1 to clear AHB_DMA_OUTFIFO_UDF_CH0_INT"]
+pub type OUTFIFO_UDF_W<'a, REG> = crate::BitWriter1C<'a, REG>;
+#[doc = "Field `OUT_AHBINF_RESP_ERR` writer - Write 1 to clear AHB_DMA_OUT_RESP_ERR_CH0_INT"]
+pub type OUT_AHBINF_RESP_ERR_W<'a, REG> = crate::BitWriter1C<'a, REG>;
 #[cfg(feature = "impl-register-debug")]
 impl core::fmt::Debug for crate::generic::Reg<CLR_SPEC> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
@@ -23,40 +23,38 @@ impl core::fmt::Debug for crate::generic::Reg<CLR_SPEC> {
 impl W {
     #[doc = "Bit 0 - Write 1 to clear AHB_DMA_OUT_DONE_CH0_INT"]
     #[inline(always)]
-    pub fn out_done_ch0_int_clr(&mut self) -> OUT_DONE_CH0_INT_CLR_W<'_, CLR_SPEC> {
-        OUT_DONE_CH0_INT_CLR_W::new(self, 0)
+    pub fn out_done(&mut self) -> OUT_DONE_W<'_, CLR_SPEC> {
+        OUT_DONE_W::new(self, 0)
     }
     #[doc = "Bit 1 - Write 1 to clear AHB_DMA_OUT_EOF_CH0_INT"]
     #[inline(always)]
-    pub fn out_eof_ch0_int_clr(&mut self) -> OUT_EOF_CH0_INT_CLR_W<'_, CLR_SPEC> {
-        OUT_EOF_CH0_INT_CLR_W::new(self, 1)
+    pub fn out_eof(&mut self) -> OUT_EOF_W<'_, CLR_SPEC> {
+        OUT_EOF_W::new(self, 1)
     }
     #[doc = "Bit 2 - Write 1 to clear AHB_DMA_OUT_DSCR_ERR_CH0_INT"]
     #[inline(always)]
-    pub fn out_dscr_err_ch0_int_clr(&mut self) -> OUT_DSCR_ERR_CH0_INT_CLR_W<'_, CLR_SPEC> {
-        OUT_DSCR_ERR_CH0_INT_CLR_W::new(self, 2)
+    pub fn out_dscr_err(&mut self) -> OUT_DSCR_ERR_W<'_, CLR_SPEC> {
+        OUT_DSCR_ERR_W::new(self, 2)
     }
     #[doc = "Bit 3 - Write 1 to clear AHB_DMA_OUT_TOTAL_EOF_CH0_INT"]
     #[inline(always)]
-    pub fn out_total_eof_ch0_int_clr(&mut self) -> OUT_TOTAL_EOF_CH0_INT_CLR_W<'_, CLR_SPEC> {
-        OUT_TOTAL_EOF_CH0_INT_CLR_W::new(self, 3)
+    pub fn out_total_eof(&mut self) -> OUT_TOTAL_EOF_W<'_, CLR_SPEC> {
+        OUT_TOTAL_EOF_W::new(self, 3)
     }
     #[doc = "Bit 4 - Write 1 to clear AHB_DMA_OUTFIFO_OVF_CH0_INT"]
     #[inline(always)]
-    pub fn outfifo_ovf_ch0_int_clr(&mut self) -> OUTFIFO_OVF_CH0_INT_CLR_W<'_, CLR_SPEC> {
-        OUTFIFO_OVF_CH0_INT_CLR_W::new(self, 4)
+    pub fn outfifo_ovf(&mut self) -> OUTFIFO_OVF_W<'_, CLR_SPEC> {
+        OUTFIFO_OVF_W::new(self, 4)
     }
     #[doc = "Bit 5 - Write 1 to clear AHB_DMA_OUTFIFO_UDF_CH0_INT"]
     #[inline(always)]
-    pub fn outfifo_udf_ch0_int_clr(&mut self) -> OUTFIFO_UDF_CH0_INT_CLR_W<'_, CLR_SPEC> {
-        OUTFIFO_UDF_CH0_INT_CLR_W::new(self, 5)
+    pub fn outfifo_udf(&mut self) -> OUTFIFO_UDF_W<'_, CLR_SPEC> {
+        OUTFIFO_UDF_W::new(self, 5)
     }
     #[doc = "Bit 6 - Write 1 to clear AHB_DMA_OUT_RESP_ERR_CH0_INT"]
     #[inline(always)]
-    pub fn out_ahbinf_resp_err_ch0_int_clr(
-        &mut self,
-    ) -> OUT_AHBINF_RESP_ERR_CH0_INT_CLR_W<'_, CLR_SPEC> {
-        OUT_AHBINF_RESP_ERR_CH0_INT_CLR_W::new(self, 6)
+    pub fn out_ahbinf_resp_err(&mut self) -> OUT_AHBINF_RESP_ERR_W<'_, CLR_SPEC> {
+        OUT_AHBINF_RESP_ERR_W::new(self, 6)
     }
 }
 #[doc = "Interrupt clear bits of TX channel 0\n\nYou can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`clr::W`](W). See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
