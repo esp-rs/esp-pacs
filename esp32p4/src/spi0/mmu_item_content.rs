8d6bf372f4cb0528ff7745c3fb5f3f6c1b4cdfcb
Björn Quentin <bjoernQ@users.noreply.github.com>
2026-07-03 10:44:49 +0200

    Align P4

--- esp32p4/src/spi0/mmu_item_content.rs
+++ esp32p4/src/spi0/mmu_item_content.rs
@@ -6,9 +6,13 @@ pub type W = crate::W<MMU_ITEM_CONTENT_SPEC>;
 pub type PADDR_R = crate::FieldReader<u16>;
 #[doc = "Field `PADDR` writer - Physical page number"]
 pub type PADDR_W<'a, REG> = crate::FieldWriter<'a, REG, 11, u16>;
-#[doc = "Field `VALID` reader - Entry is valid when set"]
+#[doc = "Field `PSRAM_VALID` reader - Entry is valid when set (PSRAM MMU)"]
+pub type PSRAM_VALID_R = crate::BitReader;
+#[doc = "Field `PSRAM_VALID` writer - Entry is valid when set (PSRAM MMU)"]
+pub type PSRAM_VALID_W<'a, REG> = crate::BitWriter<'a, REG>;
+#[doc = "Field `VALID` reader - Entry is valid when set (flash MMU)"]
 pub type VALID_R = crate::BitReader;
-#[doc = "Field `VALID` writer - Entry is valid when set"]
+#[doc = "Field `VALID` writer - Entry is valid when set (flash MMU)"]
 pub type VALID_W<'a, REG> = crate::BitWriter<'a, REG>;
 #[doc = "Field `SENSITIVE` reader - Decrypt via flash encryption when set"]
 pub type SENSITIVE_R = crate::BitReader;
@@ -20,7 +24,12 @@ impl R {
     pub fn paddr(&self) -> PADDR_R {
         PADDR_R::new((self.bits & 0x07ff) as u16)
     }
-    #[doc = "Bit 12 - Entry is valid when set"]
+    #[doc = "Bit 11 - Entry is valid when set (PSRAM MMU)"]
+    #[inline(always)]
+    pub fn psram_valid(&self) -> PSRAM_VALID_R {
+        PSRAM_VALID_R::new(((self.bits >> 11) & 1) != 0)
+    }
+    #[doc = "Bit 12 - Entry is valid when set (flash MMU)"]
     #[inline(always)]
     pub fn valid(&self) -> VALID_R {
         VALID_R::new(((self.bits >> 12) & 1) != 0)
@@ -36,6 +45,7 @@ impl core::fmt::Debug for R {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
         f.debug_struct("MMU_ITEM_CONTENT")
             .field("paddr", &self.paddr())
+            .field("psram_valid", &self.psram_valid())
             .field("valid", &self.valid())
             .field("sensitive", &self.sensitive())
             .finish()
@@ -47,7 +57,12 @@ impl W {
     pub fn paddr(&mut self) -> PADDR_W<'_, MMU_ITEM_CONTENT_SPEC> {
         PADDR_W::new(self, 0)
     }
-    #[doc = "Bit 12 - Entry is valid when set"]
+    #[doc = "Bit 11 - Entry is valid when set (PSRAM MMU)"]
+    #[inline(always)]
+    pub fn psram_valid(&mut self) -> PSRAM_VALID_W<'_, MMU_ITEM_CONTENT_SPEC> {
+        PSRAM_VALID_W::new(self, 11)
+    }
+    #[doc = "Bit 12 - Entry is valid when set (flash MMU)"]
     #[inline(always)]
     pub fn valid(&mut self) -> VALID_W<'_, MMU_ITEM_CONTENT_SPEC> {
         VALID_W::new(self, 12)
