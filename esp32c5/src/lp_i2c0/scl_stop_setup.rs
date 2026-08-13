b7a906281b159a58f2a7b1b088348a64ad02e2d4
Dániel Buga <bugadani@gmail.com>
2026-08-13 08:08:37 +0000

    Update C5 LP_I2C (#489)
    * Update C5 LP_I2C
    
    * Regenerate PAC

--- esp32c5/src/lp_i2c0/scl_stop_setup.rs
+++ esp32c5/src/lp_i2c0/scl_stop_setup.rs
@@ -1,7 +1,7 @@
-#[doc = "Register `I2C_SCL_STOP_SETUP` reader"]
-pub type R = crate::R<I2C_SCL_STOP_SETUP_SPEC>;
-#[doc = "Register `I2C_SCL_STOP_SETUP` writer"]
-pub type W = crate::W<I2C_SCL_STOP_SETUP_SPEC>;
+#[doc = "Register `SCL_STOP_SETUP` reader"]
+pub type R = crate::R<SCL_STOP_SETUP_SPEC>;
+#[doc = "Register `SCL_STOP_SETUP` writer"]
+pub type W = crate::W<SCL_STOP_SETUP_SPEC>;
 #[doc = "Field `TIME` reader - This register is used to configure the time between the positive edgeof SCL and the positive edge of SDA, in I2C module clock cycles."]
 pub type TIME_R = crate::FieldReader<u16>;
 #[doc = "Field `TIME` writer - This register is used to configure the time between the positive edgeof SCL and the positive edge of SDA, in I2C module clock cycles."]
@@ -16,7 +16,7 @@ impl R {
 #[cfg(feature = "impl-register-debug")]
 impl core::fmt::Debug for R {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-        f.debug_struct("I2C_SCL_STOP_SETUP")
+        f.debug_struct("SCL_STOP_SETUP")
             .field("time", &self.time())
             .finish()
     }
@@ -24,22 +24,22 @@ impl core::fmt::Debug for R {
 impl W {
     #[doc = "Bits 0:8 - This register is used to configure the time between the positive edgeof SCL and the positive edge of SDA, in I2C module clock cycles."]
     #[inline(always)]
-    pub fn time(&mut self) -> TIME_W<'_, I2C_SCL_STOP_SETUP_SPEC> {
+    pub fn time(&mut self) -> TIME_W<'_, SCL_STOP_SETUP_SPEC> {
         TIME_W::new(self, 0)
     }
 }
-#[doc = "Configures the delay between the SDA and SCL positive edge for a stop condition\n\nYou can [`read`](crate::Reg::read) this register and get [`i2c_scl_stop_setup::R`](R). You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`i2c_scl_stop_setup::W`](W). You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
-pub struct I2C_SCL_STOP_SETUP_SPEC;
-impl crate::RegisterSpec for I2C_SCL_STOP_SETUP_SPEC {
+#[doc = "Configures the delay between the SDA and SCL positive edge for a stop condition\n\nYou can [`read`](crate::Reg::read) this register and get [`scl_stop_setup::R`](R). You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`scl_stop_setup::W`](W). You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
+pub struct SCL_STOP_SETUP_SPEC;
+impl crate::RegisterSpec for SCL_STOP_SETUP_SPEC {
     type Ux = u32;
 }
-#[doc = "`read()` method returns [`i2c_scl_stop_setup::R`](R) reader structure"]
-impl crate::Readable for I2C_SCL_STOP_SETUP_SPEC {}
-#[doc = "`write(|w| ..)` method takes [`i2c_scl_stop_setup::W`](W) writer structure"]
-impl crate::Writable for I2C_SCL_STOP_SETUP_SPEC {
+#[doc = "`read()` method returns [`scl_stop_setup::R`](R) reader structure"]
+impl crate::Readable for SCL_STOP_SETUP_SPEC {}
+#[doc = "`write(|w| ..)` method takes [`scl_stop_setup::W`](W) writer structure"]
+impl crate::Writable for SCL_STOP_SETUP_SPEC {
     type Safety = crate::Unsafe;
 }
-#[doc = "`reset()` method sets I2C_SCL_STOP_SETUP to value 0x08"]
-impl crate::Resettable for I2C_SCL_STOP_SETUP_SPEC {
+#[doc = "`reset()` method sets SCL_STOP_SETUP to value 0x08"]
+impl crate::Resettable for SCL_STOP_SETUP_SPEC {
     const RESET_VALUE: u32 = 0x08;
 }
