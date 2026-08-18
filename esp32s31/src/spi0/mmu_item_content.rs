ff3ca9741f91b79493397e76b469abf1b7bb31b5
Dániel Buga <bugadani@gmail.com>
2026-08-18 17:08:15 +0000

    Update P4/S31 MMU_ITEM_CONTENT bits (#495)
    * Update S31 MMU_ITEM_CONTENT bits
    
    * Update P4 MEMSPI2 MMU_ITEM_CONTENT
    
    * Regenerate PACs

--- esp32s31/src/spi0/mmu_item_content.rs
+++ esp32s31/src/spi0/mmu_item_content.rs
@@ -2,30 +2,57 @@
 pub type R = crate::R<MMU_ITEM_CONTENT_SPEC>;
 #[doc = "Register `MMU_ITEM_CONTENT` writer"]
 pub type W = crate::W<MMU_ITEM_CONTENT_SPEC>;
-#[doc = "Field `paddr` reader - Physical page number of the mapped flash page"]
+#[doc = "Field `PADDR` reader - Physical page number"]
 pub type PADDR_R = crate::FieldReader<u16>;
-#[doc = "Field `paddr` writer - Physical page number of the mapped flash page"]
-pub type PADDR_W<'a, REG> = crate::FieldWriter<'a, REG, 11, u16>;
-#[doc = "Field `valid` reader - Indicates this MMU entry maps a valid flash page"]
+#[doc = "Field `PADDR` writer - Physical page number"]
+pub type PADDR_W<'a, REG> = crate::FieldWriter<'a, REG, 10, u16>;
+#[doc = "Field `ACCESS_SPIRAM` reader - Map to SPIRAM/PSRAM when set (flash when clear)"]
+pub type ACCESS_SPIRAM_R = crate::BitReader;
+#[doc = "Field `ACCESS_SPIRAM` writer - Map to SPIRAM/PSRAM when set (flash when clear)"]
+pub type ACCESS_SPIRAM_W<'a, REG> = crate::BitWriter<'a, REG>;
+#[doc = "Field `SPIRAM_VALID` reader - Entry is valid when set (SPIRAM MMU)"]
+pub type SPIRAM_VALID_R = crate::BitReader;
+#[doc = "Field `SPIRAM_VALID` writer - Entry is valid when set (SPIRAM MMU)"]
+pub type SPIRAM_VALID_W<'a, REG> = crate::BitWriter<'a, REG>;
+#[doc = "Field `VALID` reader - Entry is valid when set (flash MMU)"]
 pub type VALID_R = crate::BitReader;
-#[doc = "Field `valid` writer - Indicates this MMU entry maps a valid flash page"]
+#[doc = "Field `VALID` writer - Entry is valid when set (flash MMU)"]
 pub type VALID_W<'a, REG> = crate::BitWriter<'a, REG>;
-#[doc = "Field `sensitive` reader - Indicates this MMU entry maps an encrypted flash page"]
+#[doc = "Field `SPIRAM_SENSITIVE` reader - Decrypt via cache encryption when set (SPIRAM MMU)"]
+pub type SPIRAM_SENSITIVE_R = crate::BitReader;
+#[doc = "Field `SPIRAM_SENSITIVE` writer - Decrypt via cache encryption when set (SPIRAM MMU)"]
+pub type SPIRAM_SENSITIVE_W<'a, REG> = crate::BitWriter<'a, REG>;
+#[doc = "Field `SENSITIVE` reader - Decrypt via flash encryption when set (flash MMU)"]
 pub type SENSITIVE_R = crate::BitReader;
-#[doc = "Field `sensitive` writer - Indicates this MMU entry maps an encrypted flash page"]
+#[doc = "Field `SENSITIVE` writer - Decrypt via flash encryption when set (flash MMU)"]
 pub type SENSITIVE_W<'a, REG> = crate::BitWriter<'a, REG>;
 impl R {
-    #[doc = "Bits 0:10 - Physical page number of the mapped flash page"]
+    #[doc = "Bits 0:9 - Physical page number"]
     #[inline(always)]
     pub fn paddr(&self) -> PADDR_R {
-        PADDR_R::new((self.bits & 0x07ff) as u16)
+        PADDR_R::new((self.bits & 0x03ff) as u16)
     }
-    #[doc = "Bit 12 - Indicates this MMU entry maps a valid flash page"]
+    #[doc = "Bit 10 - Map to SPIRAM/PSRAM when set (flash when clear)"]
+    #[inline(always)]
+    pub fn access_spiram(&self) -> ACCESS_SPIRAM_R {
+        ACCESS_SPIRAM_R::new(((self.bits >> 10) & 1) != 0)
+    }
+    #[doc = "Bit 11 - Entry is valid when set (SPIRAM MMU)"]
+    #[inline(always)]
+    pub fn spiram_valid(&self) -> SPIRAM_VALID_R {
+        SPIRAM_VALID_R::new(((self.bits >> 11) & 1) != 0)
+    }
+    #[doc = "Bit 12 - Entry is valid when set (flash MMU)"]
     #[inline(always)]
     pub fn valid(&self) -> VALID_R {
         VALID_R::new(((self.bits >> 12) & 1) != 0)
     }
-    #[doc = "Bit 13 - Indicates this MMU entry maps an encrypted flash page"]
+    #[doc = "Bit 12 - Decrypt via cache encryption when set (SPIRAM MMU)"]
+    #[inline(always)]
+    pub fn spiram_sensitive(&self) -> SPIRAM_SENSITIVE_R {
+        SPIRAM_SENSITIVE_R::new(((self.bits >> 12) & 1) != 0)
+    }
+    #[doc = "Bit 13 - Decrypt via flash encryption when set (flash MMU)"]
     #[inline(always)]
     pub fn sensitive(&self) -> SENSITIVE_R {
         SENSITIVE_R::new(((self.bits >> 13) & 1) != 0)
@@ -36,23 +63,41 @@ impl core::fmt::Debug for R {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
         f.debug_struct("MMU_ITEM_CONTENT")
             .field("paddr", &self.paddr())
+            .field("access_spiram", &self.access_spiram())
+            .field("spiram_valid", &self.spiram_valid())
             .field("valid", &self.valid())
+            .field("spiram_sensitive", &self.spiram_sensitive())
             .field("sensitive", &self.sensitive())
             .finish()
     }
 }
 impl W {
-    #[doc = "Bits 0:10 - Physical page number of the mapped flash page"]
+    #[doc = "Bits 0:9 - Physical page number"]
     #[inline(always)]
     pub fn paddr(&mut self) -> PADDR_W<'_, MMU_ITEM_CONTENT_SPEC> {
         PADDR_W::new(self, 0)
     }
-    #[doc = "Bit 12 - Indicates this MMU entry maps a valid flash page"]
+    #[doc = "Bit 10 - Map to SPIRAM/PSRAM when set (flash when clear)"]
+    #[inline(always)]
+    pub fn access_spiram(&mut self) -> ACCESS_SPIRAM_W<'_, MMU_ITEM_CONTENT_SPEC> {
+        ACCESS_SPIRAM_W::new(self, 10)
+    }
+    #[doc = "Bit 11 - Entry is valid when set (SPIRAM MMU)"]
+    #[inline(always)]
+    pub fn spiram_valid(&mut self) -> SPIRAM_VALID_W<'_, MMU_ITEM_CONTENT_SPEC> {
+        SPIRAM_VALID_W::new(self, 11)
+    }
+    #[doc = "Bit 12 - Entry is valid when set (flash MMU)"]
     #[inline(always)]
     pub fn valid(&mut self) -> VALID_W<'_, MMU_ITEM_CONTENT_SPEC> {
         VALID_W::new(self, 12)
     }
-    #[doc = "Bit 13 - Indicates this MMU entry maps an encrypted flash page"]
+    #[doc = "Bit 12 - Decrypt via cache encryption when set (SPIRAM MMU)"]
+    #[inline(always)]
+    pub fn spiram_sensitive(&mut self) -> SPIRAM_SENSITIVE_W<'_, MMU_ITEM_CONTENT_SPEC> {
+        SPIRAM_SENSITIVE_W::new(self, 12)
+    }
+    #[doc = "Bit 13 - Decrypt via flash encryption when set (flash MMU)"]
     #[inline(always)]
     pub fn sensitive(&mut self) -> SENSITIVE_W<'_, MMU_ITEM_CONTENT_SPEC> {
         SENSITIVE_W::new(self, 13)
